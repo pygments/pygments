d823988bdc5b659b697c4b87d6bfa07e7befa9f2
Georg Brandl <georg@python.org>
2006-10-30 10:28:26 +0100

    [svn] Fix condensed_ruby example lexing.

--- tests/examplefiles/condensed_ruby.rb
+++ tests/examplefiles/condensed_ruby.rb
@@ -8,5 +8,3 @@ x(U)).each{|n|p,c=x(n),c(n);(c.f(p,O[0],0).map{|f|s f}-D["*"]).each{|f|F.open(f,
 "w"){|o|o<<c.f(p,f,1)}}}||(DRb.start_service U,C.new{def f(c,a=[],t=2)c==x(U)&&(
 t==0&&D[s(a)]||t==1&&F.read(s(a))||p(a))end;def y()(p(U)+p).each{|u|c(u).f(x(u),
 p(U))rescue()};self;end;private;def p(x=[]);O.push(*x).uniq!;O;end}.new.y;sleep)
-t==0&&D[s(a)]||t==1&&F.read(s(a))||p(a))end;def y()(p(U)+p).each{|u|c(u).f(x(u),
-p(U))rescue()};self;end;private;def p(x=[]);O.push(*x).uniq!;O;end}.new.y;sleep)
