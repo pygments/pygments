0938a47577fba14ee554acfd2d317f835156c9d8
Georg Brandl <georg@python.org>
2006-11-26 22:31:04 +0100

    [svn] pygments: Add Scheme lexer.

--- pygments/lexers/agile.py
+++ pygments/lexers/agile.py
@@ -3,9 +3,10 @@
     pygments.lexers.agile
     ~~~~~~~~~~~~~~~~~~~~~
 
-    Lexers for agile languages: Python, Ruby, Perl.
+    Lexers for agile languages: Python, Ruby, Perl, Scheme.
 
-    :copyright: 2006 by Georg Brandl, Armin Ronacher, Lukas Meuser.
+    :copyright: 2006 by Georg Brandl, Armin Ronacher, 
+                Lukas Meuser, Marek Kubica.
     :license: GNU LGPL, see LICENSE for more details.
 """
 
@@ -18,12 +19,13 @@ except NameError:
 from pygments.lexer import Lexer, RegexLexer, ExtendedRegexLexer, \
      LexerContext, include, combined, do_insertions, bygroups
 from pygments.token import Error, Text, \
-     Comment, Operator, Keyword, Name, String, Number, Generic
+     Comment, Operator, Keyword, Name, String, Number, Generic, Punctuation
 from pygments.util import get_bool_opt, get_list_opt, shebang_matches
 
 
 __all__ = ['PythonLexer', 'PythonConsoleLexer', 'RubyLexer',
-           'RubyConsoleLexer', 'PerlLexer', 'LuaLexer']
+           'RubyConsoleLexer', 'PerlLexer', 'LuaLexer',
+           'SchemeLexer']
 
 line_re  = re.compile('.*?\n')
 
@@ -784,3 +786,127 @@ class LuaLexer(RegexLexer):
                     yield index + len(a) + 1, Name, b
                     continue
             yield index, token, value
+
+class SchemeLexer(RegexLexer):
+    """
+    A Scheme lexer, parsing a stream and outputting the tokens
+    needed to highlight scheme code.
+    This lexer could be most probably easily subclassed to parse
+    other LISP-Dialects like Common Lisp, Emacs Lisp or AutoLisp.
+    
+    This parser is checked with pastes from the LISP pastebin
+    at http://paste.lisp.org/ to cover as much syntax as possible.
+
+    It should support the full Scheme syntax as defined in R5RS.
+    """
+    name = 'Scheme'
+    aliases = ['scheme']
+    filenames = ['*.scm']
+
+    # list of known keywords and builtins taken form vim 6.4 scheme.vim
+    # syntax file.
+    keywords = [
+        'lambda', 'define', 'if', 'else', 'cond', 'and', 'or', 'case', 'let', 
+        'let*', 'letrec', 'begin', 'do', 'delay', 'set!', '=>', 'quote',
+        'quasiquote', 'unquote', 'unquote-splicing', 'define-syntax', 
+        'let-syntax', 'letrec-syntax', 'syntax-rules'
+    ]
+    builtins = [
+        '*', '+', '-', '/', '<', '<=', '=', '>', '>=', 'abs', 'acos', 'angle',
+        'append', 'apply', 'asin', 'assoc', 'assq', 'assv', 'atan', 
+        'boolean?', 'caaaar', 'caaadr', 'caaar', 'caadar', 'caaddr', 'caadr',
+        'caar', 'cadaar', 'cadadr', 'cadar', 'caddar', 'cadddr', 'caddr', 
+        'cadr', 'call-with-current-continuation', 'call-with-input-file', 
+        'call-with-output-file', 'call-with-values', 'call/cc', 'car', 
+        'cdaaar', 'cdaadr', 'cdaar', 'cdadar', 'cdaddr', 'cdadr', 'cdar',
+        'cddaar', 'cddadr', 'cddar', 'cdddar', 'cddddr', 'cdddr', 'cddr',
+        'cdr', 'ceiling', 'char->integer', 'char-alphabetic?', 'char-ci<=?',
+        'char-ci<?', 'char-ci=?', 'char-ci>=?', 'char-ci>?', 'char-downcase',
+        'char-lower-case?', 'char-numeric?', 'char-ready?', 'char-upcase',
+        'char-upper-case?', 'char-whitespace?', 'char<=?', 'char<?', 'char=?',
+        'char>=?', 'char>?', 'char?', 'close-input-port', 'close-output-port',
+        'complex?', 'cons', 'cos', 'current-input-port', 'current-output-port',
+        'denominator', 'display', 'dynamic-wind', 'eof-object?', 'eq?', 
+        'equal?', 'eqv?', 'eval', 'even?', 'exact->inexact', 'exact?', 'exp',
+        'expt', 'floor', 'for-each', 'force', 'gcd', 'imag-part', 
+        'inexact->exact', 'inexact?', 'input-port?', 'integer->char', 
+        'integer?', 'interaction-environment', 'lcm', 'length', 'list', 
+        'list->string', 'list->vector', 'list-ref', 'list-tail', 'list?', 
+        'load', 'log', 'magnitude', 'make-polar', 'make-rectangular', 
+        'make-string', 'make-vector', 'map', 'max', 'member', 'memq', 'memv',
+        'min', 'modulo', 'negative?', 'newline', 'not', 'null-environment', 
+        'null?', 'number->string', 'number?', 'numerator', 'odd?', 
+        'open-input-file', 'open-output-file', 'output-port?', 'pair?', 
+        'peek-char', 'port?', 'positive?', 'procedure?', 'quotient', 
+        'rational?', 'rationalize', 'read', 'read-char', 'real-part', 'real?',
+        'remainder', 'reverse', 'round', 'scheme-report-environment', 
+        'set-car!', 'set-cdr!', 'sin', 'sqrt', 'string', 'string->list', 
+        'string->number', 'string->symbol', 'string-append', 'string-ci<=?', 
+        'string-ci<?', 'string-ci=?', 'string-ci>=?', 'string-ci>?', 
+        'string-copy', 'string-fill!', 'string-length', 'string-ref', 
+        'string-set!', 'string<=?', 'string<?', 'string=?', 'string>=?', 
+        'string>?', 'string?', 'substring', 'symbol->string', 'symbol?', 
+        'tan', 'transcript-off', 'transcript-on', 'truncate', 'values', 
+        'vector', 'vector->list', 'vector-fill!', 'vector-length', 
+        'vector-ref', 'vector-set!', 'vector?', 'with-input-from-file', 
+        'with-output-to-file', 'write', 'write-char', 'zero?'
+    ]
+    
+    # valid names for identifiers
+    # well, names can only not consist fully of numbers
+    # but this should be good enough for now
+    valid_name = r'[a-zA-Z0-9!$%&*+,/:<=>?@^_~-]+'
+
+    tokens = {
+        'root' : [
+            # the comments - always starting with semicolon 
+            # and going to the end of the line
+            (r';.*$', Comment.Single),
+            
+            # whitespaces - usually not relevant
+            (r'\s+', Text),
+
+            # numbers
+            (r'-?\d+\.\d+', Number.Float),
+            (r'-?\d+', Number.Integer),
+            # support for uncommon kinds of numbers - 
+            # have to figure out what the characters mean
+            #(r'(#e|#i|#b|#o|#d|#x)[\d.]+', Number),
+            
+            # strings, symbols and characters
+            (r'"(\\\\|\\"|[^"])*"', String),
+            (r"'[a-zA-Z0-9]+", String.Symbol),
+            (r'#\\[a-zA-Z0-9()]+', String.Char),
+
+            # constants
+            (r'(#t|#f)', Name.Constant),
+            
+            # special operators
+            (r"('|#|`|,@|,)", Operator),
+
+            # highlight the keywords
+            ('(%s)' % '|'.join(
+                re.escape(entry) + ' ' for entry in keywords), 
+                Keyword
+            ),
+            
+            # first variable in a quoted string like
+            # '(this is syntactic sugar)
+            (r"(?<='\()" + valid_name, Name.Variable),
+            (r"(?<=#\()" + valid_name, Name.Variable),
+            
+            # highlight the builtins
+            ("(?<=\()(%s)" % '|'.join(
+                re.escape(entry) + ' ' for entry in builtins), 
+                Name.Builtin
+            ),
+            
+            # the remaining functions  
+            (r'(?<=\()' + valid_name, Name.Function),
+            # find the remaining variables
+            (valid_name, Name.Variable),
+            
+            # the famous parantheses!
+            (r'(\(|\))', Punctuation),
+        ],
+    }
