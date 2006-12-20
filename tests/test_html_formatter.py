c8af96f89ca218c0c49b90b39fe6ea4339e39321
gbrandl <gbrandl@localhost>
2006-12-20 15:49:55 +0100

    [svn] Add a reminder to write a HTML formatter test.

---
+++ tests/test_html_formatter.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+    Pygments HTML formatter tests
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: 2006 by Georg Brandl.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import unittest
+import StringIO
+import random
+
+from pygments import lexers, formatters
+from pygments.token import _TokenType
+
+class HtmlFormatterTest(unittest.TestCase):
+
+    def test_external_css(self):
+        # TODO: write this test.
+        pass
