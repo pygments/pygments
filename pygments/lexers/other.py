dd95feb014989cd085e16237e41681623ac611dc
gbrandl <gbrandl@localhost>
2007-01-08 18:14:40 +0100

    [svn] Fix #179.

--- pygments/lexers/other.py
+++ pygments/lexers/other.py
@@ -113,7 +113,10 @@ class SqlLexer(RegexLexer):
              Name.Builtin),
             (r'[+*/<>=~!@#%^&|`?^-]', Operator),
             (r'[0-9]+', Number.Integer),
-            (r"'(''|[^'])*'", String),
+            # TODO: Backslash escapes? 
+            (r"'(''|[^'])*'", String.Single),
+            (r'"(""|[^"])*"', String.Symbol), # not a real string literal in ANSI SQL 
+            (r"`(``|[^`])*`", String.Backtick),
             (r'[a-zA-Z_][a-zA-Z0-9_]*', Name),
             (r'[;:()\[\],\.]', Punctuation)
         ],
