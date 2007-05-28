aa9e3f7924e73192a4e08eb35cce2fffd8a6b819
blackbird <blackbird@localhost>
2007-05-28 23:31:04 +0200

    [svn] added tim hatch's lexer (forgot about the CHANGELOG entry...) and updated docs to jinja 1

--- pygments/lexers/text.py
+++ pygments/lexers/text.py
@@ -17,6 +17,7 @@ try:
     set
 except NameError:
     from sets import Set as set
+from bisect import bisect
 
 from pygments.lexer import RegexLexer, bygroups, include, using, this, \
      do_insertions
@@ -27,7 +28,7 @@ from pygments.util import get_bool_opt
 
 __all__ = ['IniLexer', 'SourcesListLexer', 'MakefileLexer', 'DiffLexer',
            'IrcLogsLexer', 'TexLexer', 'GroffLexer', 'ApacheConfLexer',
-           'BBCodeLexer', 'MoinWikiLexer', 'RstLexer']
+           'BBCodeLexer', 'MoinWikiLexer', 'RstLexer', 'VimLexer']
 
 
 class IniLexer(RegexLexer):
@@ -591,3 +592,81 @@ class RstLexer(RegexLexer):
     def __init__(self, **options):
         self.handlecodeblocks = get_bool_opt(options, 'handlecodeblocks', True)
         RegexLexer.__init__(self, **options)
+
+
+class VimLexer(RegexLexer):
+    """
+    Lexer for VimL script files
+    """
+    name = 'VimL'
+    aliases = ['vim']
+    filenames = ['*.vim', '.vimrc']
+    mimetypes = ['text/x-vim']
+    flags = re.MULTILINE
+
+    tokens = {
+        'root': [
+            # Who decided that doublequote was a good comment character??
+            (r'^\s*".*', Comment),
+            (r'(?<=\s)"[^\-:.%#=*].*', Comment),
+
+            (r'[ \t]+', Text),
+            # TODO: regexes can have other delims
+            (r'/(\\\\|\\/|[^\n/])*/', String.Regex),
+            (r'"(\\\\|\\"|[^\n"])*"', String.Double),
+            (r"'(\\\\|\\'|[^\n'])*'", String.Single),
+            (r'-?\d+', Number),
+            (r'^:', Punctuation),
+            (r'[()<>+=!|,~-]', Punctuation), # Inexact list.  Looks decent.
+            (r'\b(let|if|else|endif|elseif|fun|function|endfunction)\b',
+             Keyword),
+            (r'\b\w+\b', Name.Other), # These are postprocessed below
+            (r'.', Text),
+        ],
+    }
+    def __init__(self, **options):
+        from pygments.lexers._vimbuiltins import command, option, auto
+        self._cmd = command
+        self._opt = option
+        self._aut = auto
+
+        RegexLexer.__init__(self, **options)
+
+    def is_in(self, w, mapping):
+        r"""
+        It's kind of difficult to decide if something might be a keyword
+        in VimL because it allows you to abbreviate them.  In fact,
+        'ab[breviate]' is a good example.  :ab, :abbre, or :abbreviate are
+        valid ways to call it so rather than making really awful regexps
+        like::
+
+            \bab(?:b(?:r(?:e(?:v(?:i(?:a(?:t(?:e)?)?)?)?)?)?)?)?\b
+
+        we match `\b\w+\b` and then call is_in() on those tokens.  See
+        `scripts/get_vimkw.py` for how the lists are extracted.
+        """
+        p = bisect(mapping, (w,))
+        if p > 0:
+            if mapping[p-1][0] == w[:len(mapping[p-1][0])] and \
+               mapping[p-1][1][:len(w)] == w: return True
+        if p < len(mapping):
+            return mapping[p][0] == w[:len(mapping[p][0])] and \
+                   mapping[p][1][:len(w)] == w
+        return False
+
+    def get_tokens_unprocessed(self, text):
+        # TODO: builtins are only subsequent tokens on lines
+        #       and 'keywords' only happen at the beginning except
+        #       for :au ones
+        for index, token, value in \
+            RegexLexer.get_tokens_unprocessed(self, text):
+            if token is Name.Other:
+                if self.is_in(value, self._cmd):
+                    yield index, Keyword, value
+                elif self.is_in(value, self._opt) or \
+                     self.is_in(value, self._aut):
+                    yield index, Name.Builtin, value
+                else:
+                    yield index, Text, value
+            else:
+                yield index, token, value
