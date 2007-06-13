7f39447288a3f0f6ab7ea68b9ae0cce989c48004
Georg Brandl <georg@python.org>
2007-06-13 13:38:46 +0200

    [svn] Add terminal 256 color formatter.

--- pygments/formatters/_mapping.py
+++ pygments/formatters/_mapping.py
@@ -23,6 +23,7 @@ from pygments.formatters.other import NullFormatter
 from pygments.formatters.other import RawTokenFormatter
 from pygments.formatters.rtf import RtfFormatter
 from pygments.formatters.terminal import TerminalFormatter
+from pygments.formatters.terminal256 import Terminal256Formatter
 
 FORMATTERS = {
     BBCodeFormatter: ('BBCode', ('bbcode', 'bb'), (), 'Format tokens with BBcodes. These formatting codes are used by many bulletin boards, so you can highlight your sourcecode with pygments before posting it there.'),
@@ -31,6 +32,7 @@ FORMATTERS = {
     NullFormatter: ('Text only', ('text', 'null'), ('*.txt',), 'Output the text unchanged without any formatting.'),
     RawTokenFormatter: ('Raw tokens', ('raw', 'tokens'), ('*.raw',), 'Format tokens as a raw representation for storing token streams.'),
     RtfFormatter: ('RTF', ('rtf',), ('*.rtf',), 'Format tokens as RTF markup. This formatter automatically outputs full RTF documents with color information and other useful stuff. Perfect for Copy and Paste into Microsoft\xc2\xae Word\xc2\xae documents.'),
+    Terminal256Formatter: ('Terminal256', ('terminal256', 'console256'), (), 'Format tokens with ANSI color sequences, for output in a 256-color terminal or console. Like in `TerminalFormatter` color sequences are terminated at newlines, so that paging the output works correctly.'),
     TerminalFormatter: ('Terminal', ('terminal', 'console'), (), 'Format tokens with ANSI color sequences, for output in a text console. Color sequences are terminated at newlines, so that paging the output works correctly.')
 }
 
