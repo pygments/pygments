fa88759e4bd261dd3ccccd072bd824301e85045d
Georg Brandl <georg@python.org>
2008-09-23 23:09:12 +0200

    fix 2.3 compatibility.

--- tests/support.py
+++ tests/support.py
@@ -10,6 +10,6 @@ def location(mod_name):
     """
     Return the file and directory that the code for *mod_name* is in.
     """
-    source = mod_name[:-1] if mod_name.endswith("pyc") else mod_name
+    source = mod_name.endswith("pyc") and mod_name[:-1] or mod_name
     source = os.path.abspath(source)
     return source, os.path.dirname(source)
