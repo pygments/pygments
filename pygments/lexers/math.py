ac4bc5ec957e37eb924e2e7c21b3b72edba9abf4
gbrandl <gbrandl@localhost>
2008-11-25 22:43:07 +0100

    Fix undefined global.

--- pygments/lexers/math.py
+++ pygments/lexers/math.py
@@ -16,7 +16,7 @@ try:
 except NameError:
     from sets import Set as set
 
-from pygments.lexer import Lexer, RegexLexer, bygroups, include
+from pygments.lexer import Lexer, RegexLexer, bygroups, include, do_insertions
 from pygments.token import Comment, String, Punctuation, Keyword, Name, \
     Operator, Number, Text, Generic
 
