f2450cbcfcf6f0c4cd3859fc323e411d224ed10e
gbrandl <gbrandl@localhost>
2009-03-15 23:06:05 +0100

    Modelica lexer fix from author.

--- pygments/lexers/other.py
+++ pygments/lexers/other.py
@@ -1570,11 +1570,9 @@ class ModelicaLexer(RegexLexer):
             (r'[~!%^&*+=|?:<>/-]', Operator),
             (r'[()\[\]{},.;]', Punctuation),
             (r'(true|false|NULL|Real|Integer|Boolean)\b', Name.Builtin),
-            (r"('([~!%^&*+=|?:<>/-]+|[a-zA-Z_][\w]*)'|[a-zA-Z_][\w]*)+"
-             r"(\.('([~!%^&*+=|?:<>/-]|[a-zA-Z_][\w]*)'|[a-zA-Z_][\w]*)+)+",
-             Name.Class),
-            (r"('([~!%^&*+=|?:<>/-]+|[a-zA-Z_][\w]*)'|[a-zA-Z_][\w]*)", Name)
-        ],
+            (r"([a-zA-Z_][\w]*|'[a-zA-Z_\+\-\*\/\^][\w]*')"
+             r"(\.([a-zA-Z_][\w]*|'[a-zA-Z_\+\-\*\/\^][\w]*'))+", Name.Class),
+            (r"('[\w\+\-\*\/\^]+'|\w+)", Name)        ],
         'root': [
             include('whitespace'),
             include('keywords'),
