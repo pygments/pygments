ad76c2a66cd97116f261a07bcd378a62e7962601
Tim Hatch <tim@timhatch.com>
2010-06-04 16:15:02 -0700

    Allow the license block in recently-added files to not have a starting year

--- scripts/check_sources.py
+++ scripts/check_sources.py
@@ -30,8 +30,8 @@ def checker(*suffixes, **kwds):
 
 
 name_mail_re = r'[\w ]+(<.*?>)?'
-copyright_re = re.compile(r'^    :copyright: Copyright 2006-2010 by the Pygments team, '
-                          r'see AUTHORS\.$', re.UNICODE)
+copyright_re = re.compile(r'^    :copyright: Copyright (?:\d{4}-)?2010 by '
+                          r'the Pygments team, see AUTHORS\.$', re.UNICODE)
 copyright_2_re = re.compile(r'^                %s(, %s)*[,.]$' %
                             (name_mail_re, name_mail_re), re.UNICODE)
 coding_re    = re.compile(r'coding[:=]\s*([-\w.]+)')
