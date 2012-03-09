7a6933853a689475a8fd31993c532aa1f8d049f4
Tim Hatch <tim@timhatch.com>
2012-03-09 12:06:19 -0800

    Fix a '\b' into r'\b' to match word boundary ---  pygments/lexers/templates.py |    2 +-  1 file changed, 1 insertion(+), 1 deletion(-)

--- pygments/lexers/templates.py
+++ pygments/lexers/templates.py
@@ -168,7 +168,7 @@ class SmartyLexer(RegexLexer):
             (r'#[a-zA-Z_][a-zA-Z0-9_]*#', Name.Variable),
             (r'\$[a-zA-Z_][a-zA-Z0-9_]*(\.[a-zA-Z0-9_]+)*', Name.Variable),
             (r'[~!%^&*()+=|\[\]:;,.<>/?{}@-]', Operator),
-            ('(true|false|null)\b', Keyword.Constant),
+            (r'(true|false|null)\b', Keyword.Constant),
             (r"[0-9](\.[0-9]*)?(eE[+-][0-9])?[flFLdD]?|"
              r"0[xX][0-9a-fA-F]+[Ll]?", Number),
             (r'"(\\\\|\\"|[^"])*"', String.Double),
