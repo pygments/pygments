df6ecdcc64c20451cb9a5f00faf3787530a2f351
Tim Hatch <tim@timhatch.com>
2012-03-09 12:05:34 -0800

    Fix a data loss bug in antlr lexer ---  pygments/lexers/parsers.py      |    2 +-  tests/examplefiles/antlr_throws |    1 +  2 files changed, 2 insertions(+), 1 deletion(-)  create mode 100644 tests/examplefiles/antlr_throws

--- pygments/lexers/parsers.py
+++ pygments/lexers/parsers.py
@@ -393,7 +393,7 @@ class AntlrLexer(RegexLexer):
             # throwsSpec
             (r'(throws)(\s+)(' + _id + ')',
              bygroups(Keyword, Whitespace, Name.Label)),
-            (r'(?:(,)(\s*)(' + _id + '))+',
+            (r'(,)(\s*)(' + _id + ')',
              bygroups(Punctuation, Whitespace, Name.Label)), # Additional throws
             # optionsSpec
             (r'options\b', Keyword, 'options'),
