3220e29b5f39fa2586d3333ff3e2114a45bc3560
Charles McLaughlin <mclaughlinct@gmail.com>
2012-08-14 21:57:36 -0700

    Getting started on a Puppet lexer - following the RegexLexer tutorial

---
+++ pygments/lexers/puppet.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+    pygments.lexers.puppet
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    Lexer for the Puppet DSL.
+
+    :copyright: Copyright 2006-2012 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+from pygments.lexer import RegexLexer
+from pygments.token import *
+
+__all__ = ['PuppetLexer']
+
+class PuppetLexer(RegexLexer):
+    name = 'Puppet'
+    aliases = ['puppet']
+    filenames = ['*.pp']
+
+    tokens = {
+        'root': [
+            (r' .*\n', Text),
+            (r'\+.*\n', Generic.Inserted),
+            (r'-.*\n', Generic.Deleted),
+            (r'@.*\n', Generic.Subheading),
+            (r'Index.*\n', Generic.Heading),
+            (r'=.*\n', Generic.Heading),
+            (r'.*\n', Text),
+        ]
+    }
