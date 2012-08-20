24092399e9049bbf8673b02aab9a723c59545bda
Jeffrey B. Arnold <jeffrey.arnold@gmail.com>
2012-08-20 13:07:10 -0400

    comments

--- pygments/lexers/math.py
+++ pygments/lexers/math.py
@@ -5,7 +5,7 @@
 
     Lexers for math languages.
 
-    :copyright: Copyright 2006-2012 by the Pygments team, see AUTHOR.
+    :copyright: Copyright 2006-2012 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
