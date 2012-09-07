81b08a5900ba07a4a707a1a69276076f1cef4aa7
Charles McLaughlin <mclaughlinct@gmail.com>
2012-09-06 17:47:14 -0700

    cleanup, still chipping away

--- pygments/lexers/puppet.py
+++ pygments/lexers/puppet.py
@@ -9,11 +9,16 @@
     :license: BSD, see LICENSE for details.
 """
 
-from pygments.lexer import RegexLexer, bygroups
-from pygments.token import *
+from pygments.lexer import RegexLexer, bygroups, include
+from pygments.token import Comment, Keyword, Name, Operator, Punctuation, \
+    String, Text
 
 __all__ = ['PuppetLexer']
 
+## TODO/FIXME
+# Multiline variable assignment
+# One liners line this don't look right
+#   class { 'python-custom': version => '2.7.3' }
 
 class PuppetLexer(RegexLexer):
     name = 'Puppet'
@@ -22,27 +27,46 @@ class PuppetLexer(RegexLexer):
 
     tokens = {
         'root': [
-            (r'\s*#.*$', Comment),
-            (r'/(\\\n)?[*](.|\n)*?[*](\\\n)?/', Comment.Multiline),
+            ('[a-zA-Z_][a-zA-Z0-9_]*', Name),
 
-            (r'\s+(=>|\?|<|>|=|\+|-|\/|\*|~|!|\|)\s+', Operator),
-            (r'\s+(in|and|or|not)\s+', Operator.Word),
+            include('comments'),
+            include('operators'),
+
             (r'[]{}:(),;[]', Punctuation),
 
             (r'(.*)(include)(\s*)(.*)$',
              bygroups(Text, Keyword, Text, Name.Variable)),
 
-            (r'(if|else|elsif|case|class|true|false|define)\b', Keyword),
-            (r'(inherits|notice|node|realize|import)\b', Keyword),
+            include('keywords'),
+            include('strings'),
 
-            (r'\$[^ ]*', Name.Variable),
+            (r'\$[^ ]*', Name),
 
-            (r'\'(.*?)\'', String),
-            (r'"(.*?)"', String),
-
-#            (r'(.*?)(\s*)(=>)(\s*)(.*?)$',
-#             bygroups(Name.Attribute, Text, Operator, Text, String)),
+            # FIXME puncuation at the end (such as a comma) is the wrong color
+            (r'(.*?)(\s*)(=>)(\s*)(.*?)$',
+             bygroups(Name.Attribute, Text, Operator, Text, String)),
 
             (r'[^\S\n]+', Text),
             ],
+
+        'comments': [
+            (r'\s*#.*$', Comment),
+            (r'/(\\\n)?[*](.|\n)*?[*](\\\n)?/', Comment.Multiline),
+            ],
+
+        'operators': [
+            (r'(=>|\?|<|>|=|\+|-|\/|\*|~|!|\|)', Operator),
+            (r'\s+(in|and|or|not)\s+', Operator.Word),
+            ],
+
+        'keywords': [
+            (r'(if|else|elsif|case|class|true|false|define)\b', Keyword),
+            (r'(inherits|notice|node|realize|import)\b', Keyword),
+            ],
+
+        'strings': [
+            (r'\'(.*?)\'', String),
+            (r'"(.*?)"', String),
+            ],
+
         }
