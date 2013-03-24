ef7d5630c448b3484a0f396c3eef52eb295cd8a3
Rob Hoelz <rob@hoelz.ro>
2013-03-24 11:55:37 +0100

    Allow "multi-bracket" regexes
    This isn't an offical Perl 6 term; this is a term I use to denote
    a common bug I've found in my lexer.  It involves things that look
    like this:
    
        m?< some stuff?
    
    This used to parse incorrectly.

--- pygments/lexers/agile.py
+++ pygments/lexers/agile.py
@@ -2188,7 +2188,7 @@ class Perl6Lexer(ExtendedRegexLexer):
             ( r'(?<=~~)\s*/(?:\\\\|\\/|.)*?/', String.Regex ),
             ( r'(?<=[=(,])\s*/(?:\\\\|\\/|.)*?/', String.Regex ),
             ( r'm\w+(?=[(])', Name ),
-            ( r'(?:m|ms|rx)\s*(?P<adverbs>:[\w\s:]+)?\s*(?P<delimiter>[^0-9a-zA-Z:\s]+)', brackets_callback(String.Regex) ),
+            ( r'(?:m|ms|rx)\s*(?P<adverbs>:[\w\s:]+)?\s*(?P<delimiter>(?P<first_char>[^0-9a-zA-Z:\s])(?P=first_char)*)', brackets_callback(String.Regex) ),
             ( r'(?:s|ss|tr)\s*(?::[\w\s:]+)?\s*/(?:\\\\|\\/|.)*?/(?:\\\\|\\/|.)*?/', String.Regex ),
             ( r'<[^\s=].*?\S>', String ),
             ( _build_word_match([ re.escape(x) for x in PERL6_OPERATORS]), Operator ),
