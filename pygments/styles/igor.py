5278ee57b6092d4966d402c996146eeef5ec9029
t-b <t-b@localhost>
2013-10-31 20:50:18 +0100

    Add class comment and a custom color for the decorator

--- pygments/styles/igor.py
+++ pygments/styles/igor.py
@@ -3,11 +3,15 @@ from pygments.token import Keyword, Name, Comment, String, Error, \
      Number, Operator, Generic
 
 class IgorStyle(Style):
+    """
+    Pygments version of the official colors for Igor Pro procedures.
+    """
     default_style = ""
     styles = {
         Comment:                'italic #FF0000',
         Keyword:                '#0000FF',
         Name.Function:          '#C34E00',
+        Name.Decorator:         '#CC00A3',
         Name.Class:             '#007575',
         String:                 '#009C00'
     }
