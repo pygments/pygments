627fbb87f3e489b7dc71e302d6bf0c74aa556878
Krzysztof Profic <kprofic@gmail.com>
2013-12-18 14:21:45 +0100

    Handle also cocoa protocols as keywords

--- pygments/lexers/_cocoabuiltins.py
+++ pygments/lexers/_cocoabuiltins.py
@@ -4,29 +4,37 @@ import re
 FRAMEWORKS_PATH = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS7.0.sdk/System/Library/Frameworks/'
 frameworks = os.listdir(FRAMEWORKS_PATH)
 
-all_types = set()
+all_interfaces = set()
+all_protocols  = set()
 for framework in frameworks:
 	frameworkHeadersDir = FRAMEWORKS_PATH + framework + '/Headers/'
 	if (not os.path.exists(frameworkHeadersDir)): continue
 
 	headerFilenames = os.listdir(frameworkHeadersDir)
 
-	types = set()
+	interfaces = set()
 
 	for f in headerFilenames:
 		if (not f.endswith('.h')): continue
 
 		headerFilePath = frameworkHeadersDir + f
 		content = open(headerFilePath).read()
-		res = re.search('(?<=@interface )\w+', content)
-		if (res):
-			types.add(res.group(0))
-			all_types.add(res.group(0))
+		int_res = re.search('(?<=@interface )\w+', content)
+		if (int_res):
+			interfaces.add(int_res.group(0))
+			all_interfaces.add(int_res.group(0))
 		
+		pro_res = re.search('(?<=@protocol )\w+', content)
+		if (pro_res):
+			interfaces.add(pro_res.group(0))
+			all_protocols.add(pro_res.group(0))
 
 	print framework + "\n"
 	# print types
 
 
-print "ALL: \n"
-print all_types
+print "ALL interfaces: \n"
+print all_interfaces
+
+print "\nALL protocols: \n"
+print all_protocols
