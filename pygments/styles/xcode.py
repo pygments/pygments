befc6de74801a9bfea22d646749aa5d60224c169
Krzysztof Profic <kprofic@gmail.com>
2013-12-18 16:35:07 +0100

    cleanup Xcode style

--- pygments/styles/xcode.py
+++ pygments/styles/xcode.py
@@ -3,7 +3,7 @@
     pygments.styles.xcode
     ~~~~~~~~~~~~~~~~~~~~~~
 
-    Style similar to the `Xcode`_ default style.
+    Style similar to the `Xcode` default theme.
 
     :copyright: Copyright 2006-2013 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
@@ -16,7 +16,7 @@ from pygments.token import Keyword, Name, Comment, String, Error, \
 
 class XcodeStyle(Style):
     """
-    Style similar to the Xcode default style.
+    Style similar to the Xcode default colouring theme.
     """
 
     default_style = ''
@@ -29,28 +29,20 @@ class XcodeStyle(Style):
         String.Char:            '#2300CE',
         
         Operator:               '#000000',
-        # Operator.Word:          '#ff0072',
 
         Keyword:                '#AA0D92',
-        # Keyword.Type:           '#000000',
 
         Name:                   '#000000',
         Name.Attribute:         '#836C28',
         Name.Class:             '#000000',
         Name.Function:          '#000000',
-        # Name.Property:          '#000000',
-        # Name.Namespace:         '#000000',
         Name.Builtin:           '#AA0D92',
-        Name.Builtin.Pseudo:    '#5B269A',          # In Obj-C code this token is used to colour Cocoa types, TODO new token type
-        Name.Variable:          '#000000',          # it was method argument (but not always recognized successfully)
-        # Name.Variable.Class:    '#000000',
-        # Name.Variable.Instance: '#000000',
-        # Name.Variable.Global:   '#000000',
-        # Name.Constant:          '#000000',
+        Name.Builtin.Pseudo:    '#5B269A',          # In Obj-C code this token is used to colour Cocoa types
+        Name.Variable:          '#000000',
         Name.Tag:               '#000000',
-        Name.Decorator:         '#000000',          # category name in braces
-        Name.Label:             '#000000',          # here is a little bug, it treats multiline method signatres as labels (second and later lines)
+        Name.Decorator:         '#000000',
+        Name.Label:             '#000000',          # Workaround for a BUG here: lexer treats multiline method signatres as labels
 
         Number:                 '#2300CE',
-        Error:                  '#000000',           # @ char when using as acronym for NSNumber @(10) and also in nonrecognized tokens like: @autoreleasepool, @required - only @ char
+        Error:                  '#000000',
     }
