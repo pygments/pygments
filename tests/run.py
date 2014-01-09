5559c811dffdd5d46307bd85549f9efc948bb87a
Georg Brandl <georg@python.org>
2014-01-09 19:15:06 +0100

    a friendly reminder to run setup.py build before make test on py3

--- tests/run.py
+++ tests/run.py
@@ -39,6 +39,10 @@ try:
     # make sure the current source is first on sys.path
     sys.path.insert(0, '..')
     import pygments
+except SyntaxError:
+    print ('Syntax error: %s' % sys.exc_info()[1])
+    print ('Please run setup.py build before make test on Python 3')
+    sys.exit(1)
 except ImportError:
     print ('Cannot find Pygments to test: %s' % sys.exc_info()[1])
     sys.exit(1)
