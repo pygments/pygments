48a1e43a9fd7994674982393cf047007db94007d
Georg Brandl <georg@python.org>
2014-01-09 20:15:11 +0100

    Closes #921: add binary literals to Python 2.

--- pygments/lexers/agile.py
+++ pygments/lexers/agile.py
@@ -109,6 +109,7 @@ class PythonLexer(RegexLexer):
             (r'(\d+\.\d*|\d*\.\d+)([eE][+-]?[0-9]+)?j?', Number.Float),
             (r'\d+[eE][+-]?[0-9]+j?', Number.Float),
             (r'0[0-7]+j?', Number.Oct),
+            (r'0[bB][01]+', Number.Bin),
             (r'0[xX][a-fA-F0-9]+', Number.Hex),
             (r'\d+L', Number.Integer.Long),
             (r'\d+j?', Number.Integer)
