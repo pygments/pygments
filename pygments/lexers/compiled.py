93ca33e946831fad8c8f088fc9df03069ac8abcf
Gaurav Jain <gaurav@gauravjain.org>
2014-03-22 17:56:08 -0400

    Use correct RegEx expression for whitespace

--- pygments/lexers/compiled.py
+++ pygments/lexers/compiled.py
@@ -1434,7 +1434,7 @@ def objective(baselexer):
                 # discussion in Issue 789
                 (r',', Punctuation),
                 (r'\.\.\.', Punctuation),
-                (r'(\(.*?\))( *)([a-zA-Z$_][a-zA-Z0-9$_]*)',
+                (r'(\(.*?\))(\s*)([a-zA-Z$_][a-zA-Z0-9$_]*)',
                 bygroups(using(this), Text, Name.Variable)),
                 (r'[a-zA-Z$_][a-zA-Z0-9$_]*:', Name.Function),
                 (';', Punctuation, '#pop'),
