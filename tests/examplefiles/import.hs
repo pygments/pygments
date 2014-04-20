538b3c2565ce5c240c44754561276007841377dd
Gaurav Jain <gaurav@gauravjain.org>
2014-04-20 12:40:50 -0400

    Restore order of comment regex. Instead copy better expression from root state.
    The previous commit does not handle all situations. Instead the comment regex needs to be copied from the root state. Added additional test lines in the example file for testing.

--- tests/examplefiles/import.hs
+++ tests/examplefiles/import.hs
@@ -1,7 +1,15 @@
 import "base" Data.Char
+import "base" Data.Char (isControl, isSpace)
+import "base" Data.Char (isControl, --isSpace)
+ isSpace)
+import "base" Data.Char (isControl, -- isSpace)
+ isSpace)
 
 (-->) :: Num a => a -- signature
-(-->) = 2 -- implementation
+(-->) = 2 -- >implementation
+
+--test comment
+-- test comment
 
 main :: IO ()
 main = putStrLn "hello world"
