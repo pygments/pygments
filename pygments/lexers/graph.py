282b65260a59b638a34bbe7bdf4ffc0637742811
Tim Hatch <tim@timhatch.com>
2014-04-24 10:24:21 -0400

    Start graph lexers file instead of just cypher

--- pygments/lexers/graph.py
+++ pygments/lexers/graph.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-    pygments.lexers.cypher
-    ~~~~~~~~~~~~~~~~~~~~~~
+    pygments.lexers.graph
+    ~~~~~~~~~~~~~~~~~~~~~
 
-    A Lexer for the cypher graph query language use in the neo4j graph database.
+    Lexers for graph query languages.
 
-    `CypherLexer`
-
-    the ``tests/examplefiles`` contains file "test.cyp" which has valid
-    cypher queries that execute against the example database shipped with Neo4J
-
-
-    :copyright: Copyright 2006-2013 by the Pygments team, see AUTHORS.
+    :copyright: Copyright 2006-2014 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
@@ -27,9 +21,12 @@ __all__ = ['CypherLexer']
 
 class CypherLexer(RegexLexer):
     """
-    For Cypher Query Language
-    http://docs.neo4j.org/chunked/milestone/cypher-query-lang.html
+    For `Cypher Query Language
+    <http://docs.neo4j.org/chunked/milestone/cypher-query-lang.html>`_
+
     For the Cypher version in Neo4J 2.0
+
+    .. versionadded:: 2.0
     """
     name = 'Cypher'
     aliases = ['cypher']
