f033147d7a47a416373446cb365906f4ccbfa27c
Andrew Pinkham <code@andrewsforge.com>
2014-05-06 15:20:31 -0500

    RTF Formatter: integrated tests with new string assert methods.

--- tests/test_rtf_formatter.py
+++ tests/test_rtf_formatter.py
@@ -8,12 +8,13 @@
 """
 
 import unittest
+from string_asserts import StringTests
 
 from pygments.util import StringIO
 from pygments.formatters import RtfFormatter
 from pygments.lexers.special import TextLexer
 
-class RtfFormatterTest(unittest.TestCase):
+class RtfFormatterTest(StringTests, unittest.TestCase):
 
     def format_rtf(self, t):
         tokensource = list(TextLexer().get_tokens(t))
@@ -28,19 +29,19 @@ class RtfFormatterTest(unittest.TestCase):
         t = u''
         result = self.format_rtf(t)
         expected = r'{\rtf1\ansi\uc0'
-        self.assertTrue(result.startswith(expected))
+        self.assertStartsWith(result, expected)
 
     def test_ascii_characters(self):
         t = u'a b c d ~'
         result = self.format_rtf(t)
         expected = (r'a b c d ~\par' '\n' r'}')
-        self.assertTrue(result.endswith(expected))
+        self.assertEndsWith(result, expected)
 
     def test_escape_characters(self):
         t = u'\ {{'
         result = self.format_rtf(t)
         expected = (r'\\ \{\{\par' '\n' r'}')
-        self.assertTrue(result.endswith(expected))
+        self.assertEndsWith(result, expected)
 
     def test_single_characters(self):
         t = u'â € ¤ каждой'
@@ -48,7 +49,7 @@ class RtfFormatterTest(unittest.TestCase):
         expected = (r'{\u226} {\u8364} {\u164} '
                     r'{\u1082}{\u1072}{\u1078}{\u1076}{\u1086}{\u1081}'
                     r'\par' '\n' r'}')
-        self.assertTrue(result.endswith(expected))
+        self.assertEndsWith(result, expected)
 
     def test_double_characters(self):
         t = u'က 힣 ↕ ↕︎ 鼖'
@@ -56,4 +57,4 @@ class RtfFormatterTest(unittest.TestCase):
         expected = (r'{\u4096} {\u55203} {\u8597} '
                     r'{\u8597}{\u65038} {\u55422}{\u56859}'
                     r'\par' '\n' r'}')
-        self.assertTrue(result.endswith(expected))
+        self.assertEndsWith(result, expected)
