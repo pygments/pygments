605356924cf2349578684ddd97ed4df46f223427
Gaurav Jain <gaurav@gauravjain.org>
2014-05-12 23:55:27 -0400

    Match Java function name tokens even without leading whitespace

--- tests/examplefiles/example.java
+++ tests/examplefiles/example.java
@@ -1,12 +1,11 @@
 class _PostUnicodeClassá
-{
+{void foo() {}
 
   void PostUnicodeFunctioná() {}
 }
 
 class áPreUnicodeClass
 {
-
   public int $foo;
   public int á$foo;
   _PostUnicodeClassá áPreUnicodeFunction() { return null; }
