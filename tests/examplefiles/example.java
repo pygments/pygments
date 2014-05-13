6023e9c6529f33f013f9b5f1936ce6206b08fb9e
Gaurav Jain <gaurav@gauravjain.org>
2014-05-12 23:03:21 -0400

    Add support for unicode letters in Java identifiers
    Additionally, allow $ in middle of variable names

---
+++ tests/examplefiles/example.java
@@ -0,0 +1,13 @@
+class _PostUnicodeClassá
+{
+
+  void PostUnicodeFunctioná() {}
+}
+
+class áPreUnicodeClass
+{
+
+  public int $foo;
+  public int á$foo;
+  _PostUnicodeClassá áPreUnicodeFunction() { return null; }
+}
