63c5390bd60b3f382ec12cd617551e53aa36eb7b
Gaurav Jain <gaurav@gauravjain.org>
2014-05-13 00:06:36 -0400

    Improve support $ symbol in Java identifiers

--- tests/examplefiles/example.java
+++ tests/examplefiles/example.java
@@ -1,12 +1,16 @@
-class _PostUnicodeClassá
-{void foo() {}
+class _PostUnico$deClassá
+{void fo$o() {}
 
-  void PostUnicodeFunctioná() {}
+  void PostUnicodeFunctioná() {
+  láb$el:
+    break láb$el;
+  
+  }
 }
 
-class áPreUnicodeClass
+class áPreUnicode$Class
 {
   public int $foo;
   public int á$foo;
-  _PostUnicodeClassá áPreUnicodeFunction() { return null; }
+  _PostUnico$deClassá áPreUnicodeFunction() { return null; }
 }
