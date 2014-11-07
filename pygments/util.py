16cd43ea626b6e5019292e5e82e900270e12dc55
Tim Hatch <tim@timhatch.com>
2014-11-06 19:41:33 -0800

    Add 'duplicates_removed' function (for *_builtins.py generation).

--- pygments/util.py
+++ pygments/util.py
@@ -263,6 +263,22 @@ def format_lines(var_name, seq, raw=False, indent_level=0):
     return '\n'.join(lines)
 
 
+def duplicates_removed(it, already_seen=()):
+    """
+    Returns a list with duplicates removed from the iterable `it`.
+
+    Order is preserved.
+    """
+    lst = []
+    seen = set()
+    for i in it:
+        if i in seen or i in already_seen:
+            continue
+        lst.append(i)
+        seen.add(i)
+    return lst
+
+
 class Future(object):
     """Generic class to defer some work.
 
