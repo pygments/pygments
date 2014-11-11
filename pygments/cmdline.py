15055fa66f5b0254960cb3dd8015c3ed83b08f30
Georg Brandl <georg@python.org>
2014-11-11 18:36:28 +0100

    (Hopefully) fix test_cmdline failures under Windows without colorama.

--- pygments/cmdline.py
+++ pygments/cmdline.py
@@ -445,8 +445,8 @@ def main_inner(popts, args, usage):
        fmter.name in ('Terminal', 'Terminal256'):  # pragma: no cover
         # unfortunately colorama doesn't support binary streams on Py3
         if sys.version_info > (3,):
-            import io
-            outfile = io.TextIOWrapper(outfile, encoding=fmter.encoding)
+            from pygments.util import UnclosingTextIOWrapper
+            outfile = UnclosingTextIOWrapper(outfile, encoding=fmter.encoding)
             fmter.encoding = None
         try:
             import colorama.initialise
