1df25c2c739f1960ae3ba57be4edc1c494caa5a7
Georg Brandl <georg@python.org>
2014-11-11 10:00:06 +0100

    Do not import pygments prematurely when running with coverage.

--- tests/run.py
+++ tests/run.py
@@ -28,18 +28,23 @@ except ImportError:
     print('nose is required to run the Pygments test suite')
     sys.exit(1)
 
-try:
-    # make sure the current source is first on sys.path
-    sys.path.insert(0, '..')
-    import pygments
-except SyntaxError as err:
-    print('Syntax error: %s' % err)
-    sys.exit(1)
-except ImportError as err:
-    print('Cannot find Pygments to test: %s' % err)
-    sys.exit(1)
+# make sure the current source is first on sys.path
+sys.path.insert(0, '..')
+
+if '--with-coverage' not in sys.argv:
+    # if running with coverage, pygments should not be imported before coverage
+    # is started, otherwise it will count already executed lines as uncovered
+    try:
+        import pygments
+    except ImportError as err:
+        print('Cannot find Pygments to test: %s' % err)
+        sys.exit(1)
+    else:
+        print('Pygments %s test suite running (Python %s)...' %
+              (pygments.__version__, sys.version.split()[0]),
+              file=sys.stderr)
 else:
-    print('Pygments %s test suite running (Python %s)...' %
-          (pygments.__version__, sys.version.split()[0]))
+    print('Pygments test suite running (Python %s)...' % sys.version.split()[0],
+          file=sys.stderr)
 
 nose.main()
