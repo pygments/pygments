b0a06577958c08824ff45b518c9d2e2d4444c037
James Edwards <admin@jaetech.org>
2014-12-27 18:38:08 +0000

    Added a new lexer for Terraform files.
    Should hopefully syntax highlight terraform *.tf files. Syntax is similar to json but not identical and it has it's own keywords and lookup functions. More details here: https://www.terraform.io/docs/configuration/index.html

---
+++ pygments/lexers/terraform.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+    pygments.lexers.terraform
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Lexer for Terraform tf files
+
+    :copyright: Copyright 2006-2014 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+import re
+
+from pygments.lexer import Lexer, RegexLexer, bygroups, include
+from pygments.token import Text, Comment, Operator, Keyword, Name, String, Punctuation, Number
+__all__ = ['TerraformLexer']
+
+class TerraformLexer(RegexLexer):
+    """
+    Lexer for terraformi .tf files: https://www.terraform.io/
+
+    """
+    name = 'Terraform'
+    aliases = ['terraform', 'tf']
+    filenames = ['*.tf']
+    mimetypes = ['application/x-tf', 'application/x-terraform']
+    tokens = {
+       'root': [
+            include ('string'),
+            include ('punctuation'),
+            include ('curly'),
+            include('basic'),
+            include('whitespace'),
+            (r'[0-9]+', Number),
+       ],
+       'basic': [
+            (r'\b(true|false)\b', Keyword.Type),
+            (r'\s*/\*', Comment.Multiline, 'comment'),
+            (r'\s*#.*\n', Comment.Single),
+            (r'(.*?)(\s*)(=)',
+             bygroups(Name.Attribute, Text, Operator)),
+            (r'\bvariable|resource|provider|provisioner|module\b',Keyword.Reserved,'function'),
+            (r'\bingress|egress|listener|default|connection\b',Keyword.Declaration),
+            ('\$\{',String.Interpol,'var_builtin'),
+       ],
+       'function':[
+            (r'(\s+)(".*")(\s+)', bygroups(Text,String,Text)),
+            include ('punctuation'),
+            include ('curly'),
+        ],
+        'var_builtin':[
+                (r'\$\{', String.Interpol, '#push'),
+                (r'\bconcat|file|join|lookup|element\b',Name.Builtin),
+                include ('string'),
+                include ('punctuation'),
+                (r'\s+', Text),
+                (r'\}',String.Interpol, '#pop'),
+         ],
+       'string':[
+           (r'(".*")',bygroups(String.Double)),
+        ],
+        'punctuation':[
+            (r'[\[\]\(\),.]',Punctuation),
+         ],
+        # Keep this seperate from punctuation - we sometimes want to use different
+        # Tokens for { }
+       'curly':[
+            (r'\{',Text.Punctuation),
+            (r'\}',Text.Punctuation),
+        ],
+       'comment': [
+            (r'[^*/]', Comment.Multiline),
+            (r'/\*', Comment.Multiline, '#push'),
+            (r'\*/', Comment.Multiline, '#pop'),
+            (r'[*/]', Comment.Multiline)
+        ],
+       'whitespace': [
+            (r'\n', Text),
+            (r'\s+', Text),
+            (r'\\\n', Text),
+        ],
+    }
