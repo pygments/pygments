c14a80f8cf67f90c87c171658534d5d0f761d226
Georg Brandl <georg@python.org>
2015-03-08 07:49:21 +0100

    Closes #1047: allow newlines in # comments in AutoIt.

--- pygments/lexers/automation.py
+++ pygments/lexers/automation.py
@@ -316,7 +316,8 @@ class AutoItLexer(RegexLexer):
     tokens = {
         'root': [
             (r';.*\n', Comment.Single),
-            (r'(#comments-start|#cs).*?(#comments-end|#ce)', Comment.Multiline),
+            (r'(#comments-start|#cs)(.|\n)*?(#comments-end|#ce)',
+             Comment.Multiline),
             (r'[\[\]{}(),;]', Punctuation),
             (r'(and|or|not)\b', Operator.Word),
             (r'[$|@][a-zA-Z_]\w*', Name.Variable),
