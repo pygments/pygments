262d10f9a6d54deb2e44a7d5ddb5ba38d42193b6
Tim Hatch <tim@timhatch.com>
2015-10-14 12:08:43 -0700

    Fixes noted by pylint, mostly.

--- pygments/lexers/tap.py
+++ pygments/lexers/tap.py
@@ -5,7 +5,7 @@
 
     Lexer for the Test Anything Protocol (TAP).
 
-    :copyright: Copyright 2006-2014 by the Pygments team, see AUTHORS.
+    :copyright: Copyright 2006-2015 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
