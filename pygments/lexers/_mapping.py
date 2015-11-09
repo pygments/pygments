44f2a3e05bf284d092dc3235d9d761d7429867b6
hhsprings <xwhhsprings@gmail.com>
2015-11-09 15:48:31 +0900

    move `.replace` immediately after `read()`, and explain in a comment that it's for windows.

--- pygments/lexers/_mapping.py
+++ pygments/lexers/_mapping.py
@@ -440,13 +440,20 @@ if __name__ == '__main__':  # pragma: no cover
     # extract useful sourcecode from this file
     with open(__file__) as fp:
         content = fp.read()
+        # replace crnl to nl for Windows.
+        #
+        # Note that, originally, contributers should keep nl of master
+        # repository, for example by using some kind of automatic
+        # management EOL, like `EolExtension
+        #  <https://www.mercurial-scm.org/wiki/EolExtension>`.
+        content = content.replace("\r\n", "\n")
     header = content[:content.find('LEXERS = {')]
     footer = content[content.find("if __name__ == '__main__':"):]
 
     # write new file
     with open(__file__, 'wb') as fp:
-        fp.write(header.replace("\r\n", "\n"))
+        fp.write(header)
         fp.write('LEXERS = {\n    %s,\n}\n\n' % ',\n    '.join(found_lexers))
-        fp.write(footer.replace("\r\n", "\n"))
+        fp.write(footer)
 
     print ('=== %d lexers processed.' % len(found_lexers))
