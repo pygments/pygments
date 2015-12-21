e5d95f923026c38c88d48eccba151931e76d7f95
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-12-21 22:03:59 +0100

    Revert unintentional change to copyright

--- pygments/style.py
+++ pygments/style.py
@@ -5,7 +5,7 @@
 
     Basic style object.
 
-    :copyright: Copyright 2006-2014 by the Pygments team, see AUTHORS.
+    :copyright: Copyright 2006-2015 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
