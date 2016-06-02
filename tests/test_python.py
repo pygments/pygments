6f099f825e0c134ade8c1b48a67830fe354fbca9
Jessie Wincek <jbwincek@gmail.com>
2016-06-02 10:47:17 -0700

    Added Python cls builtin word token recognition

---
+++ tests/test_python.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+    Python Tests
+    ~~~~~~~~~
+
+    :copyright: Copyright 2006-2015 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+import unittest
+
+from pygments.lexers import PythonLexer
+from pygments.token import Token
+
+
+class PythonTest(unittest.TestCase):
+    def setUp(self):
+        self.lexer = PythonLexer()
+
+    def testNeedsName(self):
+        fragment = 'class TestClass():\n    @classmethod\n    def hello(cls):\n        pass\n'
+        tokens = [
+            (Token.Keyword, 'class'),
+            (Token.Text, ' '),
+            (Token.Name.Class, 'TestClass'),
+            (Token.Punctuation, '('),
+            (Token.Punctuation, ')'),
+            (Token.Punctuation, ':'),
+            (Token.Text, '\n'),
+            (Token.Text, '    '),
+            (Token.Name.Decorator, '@classmethod'),
+            (Token.Text, '\n'),
+            (Token.Text, '    '),
+            (Token.Keyword, 'def'),
+            (Token.Text, ' '),
+            (Token.Name.Function, 'hello'),
+            (Token.Punctuation, '('),
+            (Token.Name.Builtin.Pseudo, 'cls'),
+            (Token.Punctuation, ')'),
+            (Token.Punctuation, ':'),
+            (Token.Text, '\n'),
+            (Token.Text, '        '),
+            (Token.Keyword, 'pass'),
+            (Token.Text, '\n'),
+        ]
+        self.assertEqual(tokens, list(self.lexer.get_tokens(fragment)))
