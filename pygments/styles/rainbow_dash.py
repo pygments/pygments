0730d162c5dec55244f74e2220a12867e2480533
Georg Brandl <georg@python.org>
2017-01-22 18:40:53 +0100

    styles: fixups

--- pygments/styles/rainbow_dash.py
+++ pygments/styles/rainbow_dash.py
@@ -7,7 +7,7 @@
 
     .. _theme: http://sanssecours.github.io/Rainbow-Dash.tmbundle
 
-    :copyright: Copyright 2006-2016 by the Pygments team, see AUTHORS.
+    :copyright: Copyright 2006-2017 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
