00a4dae881b0918dc1a67885d58dfea1e01dcebe
Georg Brandl <georg@python.org>
2017-01-22 19:10:32 +0100

    More housekeeping and missed additions to CHANGES.

--- pygments/lexers/grammar_notation.py
+++ pygments/lexers/grammar_notation.py
@@ -13,7 +13,7 @@ import re
 
 from pygments.lexer import RegexLexer, bygroups, include, this, using, words
 from pygments.token import Comment, Keyword, Literal, Name, Number, \
-     Operator, Punctuation, String, Text
+    Operator, Punctuation, String, Text
 
 __all__ = ['BnfLexer', 'AbnfLexer', 'JsgfLexer']
 
