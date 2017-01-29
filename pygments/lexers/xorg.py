58adfa4bcaf38c86c4e0efbb051b969175763522
Georg Brandl <georg@python.org>
2017-01-29 08:30:26 +0100

    Xorg: fixup style, add test file, regenerate mapping.

--- pygments/lexers/xorg.py
+++ pygments/lexers/xorg.py
@@ -1,33 +1,34 @@
 # -*- coding: utf-8 -*-
 """
     pygments.lexers.xorg
-    ~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~
 
     Lexers for Xorg configs.
 
-    :copyright: Copyright 2006-2016 by the Pygments team, see AUTHORS.
+    :copyright: Copyright 2006-2017 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
-import re
-
 from pygments.lexer import RegexLexer, bygroups
-from pygments.token import *
+from pygments.token import Comment, String, Name, Text
 
 __all__ = ['XorgLexer']
 
+
 class XorgLexer(RegexLexer):
-	name = 'Xorg'
-	aliases = ['xorg.conf']
-	filenames = []
-	mimetypes = []
+    name = 'Xorg'
+    aliases = ['xorg.conf']
+    filenames = ['xorg.conf']
+    mimetypes = []
 
-	tokens = {
-		'root': [
-			(r'#.*$', Comment),
-			(r'((|Sub)Section)(\s+)("\w+")', bygroups (String.Escape, String.Escape, Whitespace, String.Escape)),
-			(r'(End(|Sub)Section)', String.Escape),
+    tokens = {
+        'root': [
+            (r'#.*$', Comment),
+            (r'((|Sub)Section)(\s+)("\w+")',
+             bygroups(String.Escape, String.Escape, Text, String.Escape)),
+            (r'(End(|Sub)Section)', String.Escape),
 
-			(r'(^(?!S|E|#)(|\s+)(?!Sec|End|Sub)\w+)(\s+)([^\n#]+)', bygroups (Name.Builtin, Whitespace, Whitespace, Name.Constant)),
-		]
-	}
+            (r'(^(?!S|E|#)(|\s+)(?!Sec|End|Sub)\w+)(\s+)([^\n#]+)',
+             bygroups(Name.Builtin, Text, Text, Name.Constant)),
+        ],
+    }
