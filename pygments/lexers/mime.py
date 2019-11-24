ef34358f6a5ef69d06108feeddfd5e86915475db
Georg Brandl <georg@python.org>
2019-11-24 18:49:49 +0100

    misc: fix synthesized tokens not using text_type (unicode)
    Fixes the test failure seen at https://github.com/pygments/pygments/runs/318264350

--- pygments/lexers/mime.py
+++ pygments/lexers/mime.py
@@ -99,8 +99,8 @@ class MIMELexer(RegexLexer):
         entire_body = match.group()
 
         # skip first newline
-        if entire_body[0] =='\n':
-            yield pos_body_start, Text.Whitespace, '\n'
+        if entire_body[0] == '\n':
+            yield pos_body_start, Text.Whitespace, u'\n'
             pos_body_start = pos_body_start + 1
             entire_body = entire_body[1:]
 
@@ -176,7 +176,7 @@ class MIMELexer(RegexLexer):
         prefix_len = match.start(1) - match.start(0)
         yield match.start(0), Text.Whitespace, match.group(0)[:prefix_len]
         yield match.start(1), Name.Label, match.group(2)
-        yield match.end(2), String.Delimiter, "/"
+        yield match.end(2), String.Delimiter, u"/"
         yield match.start(3), Name.Label, match.group(3)
 
     def get_content_type_subtokens(self, match):
