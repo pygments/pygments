pygments/pygments

commit 8d0828bbfc5b8deb7525a19e1037704f644563da

Exclude tests from distribution.
The tests package is found as a package due to presence of __init__.py. Use include to ensure only pygments gets installed.

setup.py
@@ -33,7 +33,7 @@ setup(
     description = 'Pygments is a syntax highlighting package written in Python.',
     long_description = __doc__,
     keywords = 'syntax highlighting',
-    packages = find_packages(),
+    packages = find_packages(include=['pygments']),
     entry_points = {
         'console_scripts': ['pygmentize = pygments.cmdline:main'],
     },

commit 954fd935bb47fefa8981e65d74a22464ace9ad5b

Ensure `setup.py` works again
While `pip3 install .` still works, `python3 setup.py install` does not (and
ditto for Python2), because subpackages of `pygments` don't get installed. Even
running `find_packages` on its own has consistent behavior:

```
$ python3
>>> from setuptools import setup, find_packages
>>> find_packages(exclude=['tests'])
['pygments', 'pygments.filters', 'pygments.lexers', 'pygments.formatters', 'pygments.styles']
>>> find_packages(include=['pygments'])
['pygments']
```

The bug seems to have been introduced in
8d0828bbfc5b8deb7525a19e1037704f644563da.

Tested on macOS 10.14 with Homebrew Python 2.7.17 and 3.7.5.

setup.py
@@ -33,7 +33,7 @@ setup(
     description = 'Pygments is a syntax highlighting package written in Python.',
     long_description = __doc__,
     keywords = 'syntax highlighting',
-    packages = find_packages(include=['pygments']),
+    packages = find_packages(exclude=['tests']),
     entry_points = {
         'console_scripts': ['pygmentize = pygments.cmdline:main'],
     },