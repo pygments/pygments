a03870f872eb32c4ea75d177494b817dcf2c68d5
Colin Kennedy <colinvfx@gmail.com>
2019-11-30 22:12:06 -0800

    Ran auto-fix on test_usd.py

--- tests/test_usd.py
+++ tests/test_usd.py
@@ -6,8 +6,7 @@
 import textwrap
 import unittest
 
-from pygments import lexers
-from pygments import token
+from pygments import lexers, token
 
 
 class UsdTest(unittest.TestCase):
@@ -18,256 +17,254 @@ class UsdTest(unittest.TestCase):
         return list(self.lexer.get_tokens(code))
 
     def test_asset_path(self):
-        code = '@/some/path/to/a/file/foo.usda@'
+        code = "@/some/path/to/a/file/foo.usda@"
         expected = [
             (token.String.Interpol, code),
-            (token.Text, '\n'),
+            (token.Text, "\n"),
         ]
 
         self.assertEqual(expected, self._get(code))
 
     def test_asset_path_uri(self):
-        code = r'@file://SPECI__Z-_ALIZED(syntax_here)?with_arbitrary@#)(%*&)\characters.tar.gz@'
+        code = r"@file://SPECI__Z-_ALIZED(syntax_here)?with_arbitrary@#)(%*&)\characters.tar.gz@"
         expected = [
             (token.String.Interpol, code),
-            (token.Text, '\n'),
+            (token.Text, "\n"),
         ]
 
         self.assertEqual(expected, self._get(code))
 
     def test_target_absolute(self):
         for code in [
-            '</some/path/here>',
-            '</some/another_one/here>',
-            '</some/path/here.property_name>',
+            "</some/path/here>",
+            "</some/another_one/here>",
+            "</some/path/here.property_name>",
         ]:
             self.assertEqual(
-                [(token.Name.Namespace, code), (token.Text, '\n')],
-                self._get(code),
+                [(token.Name.Namespace, code), (token.Text, "\n")], self._get(code),
             )
 
     def test_target_relative(self):
         for code in [
-            '<../some/path/here>',
-            '<../some/another_one/here>',
-            '<../some/path/here.property_name>',
+            "<../some/path/here>",
+            "<../some/another_one/here>",
+            "<../some/path/here.property_name>",
         ]:
             self.assertEqual(
-                [(token.Name.Namespace, code), (token.Text, '\n')],
-                self._get(code),
+                [(token.Name.Namespace, code), (token.Text, "\n")], self._get(code),
             )
 
     def test_attribute(self):
-        normal = 'double foo = 8.0'
+        normal = "double foo = 8.0"
 
         self.assertEqual(
             [
-                (token.Token.Keyword.Type, 'double'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Name.Attribute, 'foo'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Operator, '='),
-                (token.Token.Text, ' '),
-                (token.Token.Literal.Number, '8.0'),
-                (token.Token.Text, '\n'),
+                (token.Token.Keyword.Type, "double"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Name.Attribute, "foo"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Operator, "="),
+                (token.Token.Text, " "),
+                (token.Token.Literal.Number, "8.0"),
+                (token.Token.Text, "\n"),
             ],
             self._get(normal),
         )
 
-        custom = 'custom double foo = 8.0'
+        custom = "custom double foo = 8.0"
 
         self.assertEqual(
             [
-                (token.Token.Keyword.Token, 'custom'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Keyword.Type, 'double'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Name.Attribute, 'foo'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Operator, '='),
-                (token.Token.Text, ' '),
-                (token.Token.Literal.Number, '8.0'),
-                (token.Token.Text, '\n'),
+                (token.Token.Keyword.Token, "custom"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Keyword.Type, "double"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Name.Attribute, "foo"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Operator, "="),
+                (token.Token.Text, " "),
+                (token.Token.Literal.Number, "8.0"),
+                (token.Token.Text, "\n"),
             ],
             self._get(custom),
         )
 
-        uniform = 'uniform double foo = 8.0'
+        uniform = "uniform double foo = 8.0"
 
         self.assertEqual(
             [
-                (token.Token.Keyword.Token, 'uniform'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Keyword.Type, 'double'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Name.Attribute, 'foo'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Operator, '='),
-                (token.Token.Text, ' '),
-                (token.Token.Literal.Number, '8.0'),
-                (token.Token.Text, '\n'),
+                (token.Token.Keyword.Token, "uniform"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Keyword.Type, "double"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Name.Attribute, "foo"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Operator, "="),
+                (token.Token.Text, " "),
+                (token.Token.Literal.Number, "8.0"),
+                (token.Token.Text, "\n"),
             ],
             self._get(uniform),
         )
 
-        custom_uniform = 'custom uniform double foo = 8.0'
+        custom_uniform = "custom uniform double foo = 8.0"
 
         self.assertEqual(
             [
-                (token.Token.Keyword.Token, 'custom'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Keyword.Token, 'uniform'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Keyword.Type, 'double'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Name.Attribute, 'foo'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Operator, '='),
-                (token.Token.Text, ' '),
-                (token.Token.Literal.Number, '8.0'),
-                (token.Token.Text, '\n'),
+                (token.Token.Keyword.Token, "custom"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Keyword.Token, "uniform"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Keyword.Type, "double"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Name.Attribute, "foo"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Operator, "="),
+                (token.Token.Text, " "),
+                (token.Token.Literal.Number, "8.0"),
+                (token.Token.Text, "\n"),
             ],
             self._get(custom_uniform),
         )
 
-        underscore = 'custom double foo_underscore_name = 8.0'
+        underscore = "custom double foo_underscore_name = 8.0"
 
         self.assertEqual(
             [
-                (token.Token.Keyword.Token, 'custom'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Keyword.Type, 'double'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Name.Attribute, 'foo_underscore_name'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Operator, '='),
-                (token.Token.Text, ' '),
-                (token.Token.Literal.Number, '8.0'),
-                (token.Token.Text, '\n'),
+                (token.Token.Keyword.Token, "custom"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Keyword.Type, "double"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Name.Attribute, "foo_underscore_name"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Operator, "="),
+                (token.Token.Text, " "),
+                (token.Token.Literal.Number, "8.0"),
+                (token.Token.Text, "\n"),
             ],
             self._get(underscore),
         )
 
-        array = 'double[] foo_underscore_name = [10.1, 12.0, 13]'
+        array = "double[] foo_underscore_name = [10.1, 12.0, 13]"
 
         self.assertEqual(
             [
-                (token.Token.Keyword.Type, 'double[]'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Name.Attribute, 'foo_underscore_name'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Operator, '='),
-                (token.Token.Text, ' '),
-                (token.Token.Punctuation, '['),
-                (token.Token.Literal.Number, '10.1'),
-                (token.Token.Generic, ','),
-                (token.Token.Text, ' '),
-                (token.Token.Literal.Number, '12.0'),
-                (token.Token.Generic, ','),
-                (token.Token.Text, ' '),
-                (token.Token.Literal.Number, '13'),
-                (token.Token.Punctuation, ']'),
-                (token.Token.Text, '\n'),
+                (token.Token.Keyword.Type, "double[]"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Name.Attribute, "foo_underscore_name"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Operator, "="),
+                (token.Token.Text, " "),
+                (token.Token.Punctuation, "["),
+                (token.Token.Literal.Number, "10.1"),
+                (token.Token.Generic, ","),
+                (token.Token.Text, " "),
+                (token.Token.Literal.Number, "12.0"),
+                (token.Token.Generic, ","),
+                (token.Token.Text, " "),
+                (token.Token.Literal.Number, "13"),
+                (token.Token.Punctuation, "]"),
+                (token.Token.Text, "\n"),
             ],
             self._get(array),
         )
 
-        namespaced = 'double[] primvar:foo_thing = [10.1, 12.0, 13]'
+        namespaced = "double[] primvar:foo_thing = [10.1, 12.0, 13]"
 
         self.assertEqual(
             [
-                (token.Token.Keyword.Type, 'double[]'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Name.Attribute, 'primvar:foo_thing'),
-                (token.Token.Text.Whitespace, ' '),
-                (token.Token.Operator, '='),
-                (token.Token.Text, ' '),
-                (token.Token.Punctuation, '['),
-                (token.Token.Literal.Number, '10.1'),
-                (token.Token.Generic, ','),
-                (token.Token.Text, ' '),
-                (token.Token.Literal.Number, '12.0'),
-                (token.Token.Generic, ','),
-                (token.Token.Text, ' '),
-                (token.Token.Literal.Number, '13'),
-                (token.Token.Punctuation, ']'),
-                (token.Token.Text, '\n'),
+                (token.Token.Keyword.Type, "double[]"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Name.Attribute, "primvar:foo_thing"),
+                (token.Token.Text.Whitespace, " "),
+                (token.Token.Operator, "="),
+                (token.Token.Text, " "),
+                (token.Token.Punctuation, "["),
+                (token.Token.Literal.Number, "10.1"),
+                (token.Token.Generic, ","),
+                (token.Token.Text, " "),
+                (token.Token.Literal.Number, "12.0"),
+                (token.Token.Generic, ","),
+                (token.Token.Text, " "),
+                (token.Token.Literal.Number, "13"),
+                (token.Token.Punctuation, "]"),
+                (token.Token.Text, "\n"),
             ],
             self._get(namespaced),
         )
 
         timesamples = textwrap.dedent(
-            '''\
+            """\ 
             custom int[] foo = [8, 10, 14]
             custom int[] foo.timeSamples = {
                 1: [8, 0, 14],
                 2: [-8, 0, 14],
             }
-            '''
+            """
         )
 
         self.assertEqual(
             [
-                (token.Token.Keyword.Token, u'custom'),
-                (token.Token.Text.Whitespace, u' '),
-                (token.Token.Keyword.Type, u'int[]'),
-                (token.Token.Text.Whitespace, u' '),
-                (token.Token.Name.Attribute, u'foo'),
-                (token.Token.Text.Whitespace, u' '),
-                (token.Token.Operator, u'='),
-                (token.Token.Text, u' '),
-                (token.Token.Punctuation, u'['),
-                (token.Token.Literal.Number, u'8'),
-                (token.Token.Generic, u','),
-                (token.Token.Text, u' '),
-                (token.Token.Literal.Number, u'10'),
-                (token.Token.Generic, u','),
-                (token.Token.Text, u' '),
-                (token.Token.Literal.Number, u'14'),
-                (token.Token.Punctuation, u']'),
-                (token.Token.Text, u'\n'),
-                (token.Token.Keyword.Token, u'custom'),
-                (token.Token.Text.Whitespace, u' '),
-                (token.Token.Keyword.Type, u'int[]'),
-                (token.Token.Text.Whitespace, u' '),
-                (token.Token.Name.Attribute, u'foo'),
-                (token.Generic, u'.'),
-                (token.Name.Keyword.Tokens, u'timeSamples'),
-                (token.Token.Text.Whitespace, u' '),
-                (token.Token.Operator, u'='),
-                (token.Token.Text, u' '),
-                (token.Token.Punctuation, u'{'),
-                (token.Token.Text, u'\n    '),
-                (token.Token.Literal.Number, u'1'),
-                (token.Token.Generic, u':'),
-                (token.Token.Text, u' '),
-                (token.Token.Punctuation, u'['),
-                (token.Token.Literal.Number, u'8'),
-                (token.Token.Generic, u','),
-                (token.Token.Text, u' '),
-                (token.Token.Literal.Number, u'0'),
-                (token.Token.Generic, u','),
-                (token.Token.Text, u' '),
-                (token.Token.Literal.Number, u'14'),
-                (token.Token.Punctuation, u']'),
-                (token.Token.Generic, u','),
-                (token.Token.Text, u'\n    '),
-                (token.Token.Literal.Number, u'2'),
-                (token.Token.Generic, u':'),
-                (token.Token.Text, u' '),
-                (token.Token.Punctuation, u'['),
-                (token.Token.Literal.Number, u'-8'),
-                (token.Token.Generic, u','),
-                (token.Token.Text, u' '),
-                (token.Token.Literal.Number, u'0'),
-                (token.Token.Generic, u','),
-                (token.Token.Text, u' '),
-                (token.Token.Literal.Number, u'14'),
-                (token.Token.Punctuation, u']'),
-                (token.Token.Generic, u','),
-                (token.Token.Text, u'\n'),
-                (token.Token.Punctuation, u'}'),
-                (token.Token.Text, u'\n'),
+                (token.Token.Keyword.Token, u"custom"),
+                (token.Token.Text.Whitespace, u" "),
+                (token.Token.Keyword.Type, u"int[]"),
+                (token.Token.Text.Whitespace, u" "),
+                (token.Token.Name.Attribute, u"foo"),
+                (token.Token.Text.Whitespace, u" "),
+                (token.Token.Operator, u"="),
+                (token.Token.Text, u" "),
+                (token.Token.Punctuation, u"["),
+                (token.Token.Literal.Number, u"8"),
+                (token.Token.Generic, u","),
+                (token.Token.Text, u" "),
+                (token.Token.Literal.Number, u"10"),
+                (token.Token.Generic, u","),
+                (token.Token.Text, u" "),
+                (token.Token.Literal.Number, u"14"),
+                (token.Token.Punctuation, u"]"),
+                (token.Token.Text, u"\n"),
+                (token.Token.Keyword.Token, u"custom"),
+                (token.Token.Text.Whitespace, u" "),
+                (token.Token.Keyword.Type, u"int[]"),
+                (token.Token.Text.Whitespace, u" "),
+                (token.Token.Name.Attribute, u"foo"),
+                (token.Generic, u"."),
+                (token.Name.Keyword.Tokens, u"timeSamples"),
+                (token.Token.Text.Whitespace, u" "),
+                (token.Token.Operator, u"="),
+                (token.Token.Text, u" "),
+                (token.Token.Punctuation, u"{"),
+                (token.Token.Text, u"\n    "),
+                (token.Token.Literal.Number, u"1"),
+                (token.Token.Generic, u":"),
+                (token.Token.Text, u" "),
+                (token.Token.Punctuation, u"["),
+                (token.Token.Literal.Number, u"8"),
+                (token.Token.Generic, u","),
+                (token.Token.Text, u" "),
+                (token.Token.Literal.Number, u"0"),
+                (token.Token.Generic, u","),
+                (token.Token.Text, u" "),
+                (token.Token.Literal.Number, u"14"),
+                (token.Token.Punctuation, u"]"),
+                (token.Token.Generic, u","),
+                (token.Token.Text, u"\n    "),
+                (token.Token.Literal.Number, u"2"),
+                (token.Token.Generic, u":"),
+                (token.Token.Text, u" "),
+                (token.Token.Punctuation, u"["),
+                (token.Token.Literal.Number, u"-8"),
+                (token.Token.Generic, u","),
+                (token.Token.Text, u" "),
+                (token.Token.Literal.Number, u"0"),
+                (token.Token.Generic, u","),
+                (token.Token.Text, u" "),
+                (token.Token.Literal.Number, u"14"),
+                (token.Token.Punctuation, u"]"),
+                (token.Token.Generic, u","),
+                (token.Token.Text, u"\n"),
+                (token.Token.Punctuation, u"}"),
+                (token.Token.Text, u"\n"),
             ],
             self._get(timesamples),
         )
@@ -276,19 +273,19 @@ class UsdTest(unittest.TestCase):
     #     raise NotImplementedError()
 
     def test_numbers(self):
-        code = '8 8.0123312132, -4 -14.123'
+        code = "8 8.0123312132, -4 -14.123"
 
         self.assertEqual(
             [
-                (token.Token.Literal.Number, u'8'),
-                (token.Token.Text, u' '),
-                (token.Token.Literal.Number, u'8.0123312132'),
-                (token.Token.Generic, u','),
-                (token.Token.Text, u' '),
-                (token.Token.Literal.Number, u'-4'),
-                (token.Token.Text, u' '),
-                (token.Token.Literal.Number, u'-14.123'),
-                (token.Token.Text, u'\n'),
+                (token.Token.Literal.Number, u"8"),
+                (token.Token.Text, u" "),
+                (token.Token.Literal.Number, u"8.0123312132"),
+                (token.Token.Generic, u","),
+                (token.Token.Text, u" "),
+                (token.Token.Literal.Number, u"-4"),
+                (token.Token.Text, u" "),
+                (token.Token.Literal.Number, u"-14.123"),
+                (token.Token.Text, u"\n"),
             ],
             self._get(code),
         )
