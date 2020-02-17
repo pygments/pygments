6663931728b4037caf79278e825670bf0f26f941
Edgar Lee <edgarl@netflix.com>
2020-02-17 10:49:33 -0800

    Update HLB lexer for non-capturing group for non-builtin function calls

--- pygments/lexers/hlb.py
+++ pygments/lexers/hlb.py
@@ -52,7 +52,7 @@ class HlbLexer(RegexLexer):
             (u'(with)', bygroups(Keyword.Reserved)),
             (u'(as)([\\t ]+)(\\b[a-zA-Z_][a-zA-Z0-9]*\\b)', bygroups(Keyword.Reserved, String, Name.Function)),
             (u'(\\bstring\\b|\\bint\\b|\\bbool\\b|\\bfs\\b|\\boption\\b)([\\t ]+)(\\{)', bygroups(Keyword.Type, Text, Punctuation), 'block'),
-            (u'(\\b((?!(scratch|image|resolve|http|checksum|chmod|filename|git|keepGitDir|local|includePatterns|excludePatterns|followPaths|generate|frontendInput|shell|run|readonlyRootfs|env|dir|user|network|security|host|ssh|secret|mount|target|localPath|uid|gid|mode|readonly|tmpfs|sourcePath|cache|mkdir|createParents|chown|createdTime|mkfile|rm|allowNotFound|allowWildcards|copy|followSymlinks|contentsOnly|unpack|createDestPath)\\b)[a-zA-Z_][a-zA-Z0-9]*\\b))', bygroups(Name.Variable)),
+            (u'(?!\\b(?:scratch|image|resolve|http|checksum|chmod|filename|git|keepGitDir|local|includePatterns|excludePatterns|followPaths|generate|frontendInput|shell|run|readonlyRootfs|env|dir|user|network|security|host|ssh|secret|mount|target|localPath|uid|gid|mode|readonly|tmpfs|sourcePath|cache|mkdir|createParents|chown|createdTime|mkfile|rm|allowNotFound|allowWildcards|copy|followSymlinks|contentsOnly|unpack|createDestPath)\\b)(\\b[a-zA-Z_][a-zA-Z0-9]*\\b)', bygroups(Name.Variable)),
             ('(\n|\r|\r\n)', Text),
             ('.', Text),
         ], 
