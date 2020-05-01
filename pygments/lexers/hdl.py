e1d85b614addf3564ad253a07574f3f603a68f49
Chris Drake <cjdrake@gmail.com>
2020-05-01 04:01:37 -0700

    Fix typo: change 'wo' to 'wor'
    There is no 'wo' keyword. This was probably supposed to be
    'wor', short for "wired OR".

--- pygments/lexers/hdl.py
+++ pygments/lexers/hdl.py
@@ -311,7 +311,7 @@ class SystemVerilogLexer(RegexLexer):
             (words((
                 'byte', 'shortint', 'int', 'longint', 'integer', 'time',
                 'bit', 'logic', 'reg', 'supply0', 'supply1', 'tri', 'triand',
-                'trior', 'tri0', 'tri1', 'trireg', 'uwire', 'wire', 'wand', 'wo'
+                'trior', 'tri0', 'tri1', 'trireg', 'uwire', 'wire', 'wand', 'wor'
                 'shortreal', 'real', 'realtime'), suffix=r'\b'),
              Keyword.Type),
             (r'[a-zA-Z_]\w*:(?!:)', Name.Label),
