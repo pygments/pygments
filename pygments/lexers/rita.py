67c5879279daa5744101d4f3aa21b3b5c7f88388
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2020-09-08 15:46:45 +0300

    Initial version

---
+++ pygments/lexers/rita.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+    pygments.lexers.rita
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    Lexers for RITA language
+
+    :copyright: Copyright 2006-2020 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+import re
+
+from pygments.lexer import RegexLexer, include, bygroups, using, this, \
+    inherit, words
+from pygments.token import Comment, Operator, Keyword, Name, Literal, Punctuation, Text
+
+__all__ = ['RitaLexer']
+
+
+class RitaLexer(RegexLexer):
+    name = 'Rita'
+    filenames = ['*.rita']
+    aliases = ['rita']
+    mimetypes = ['text/rita']
+
+    flags = re.MULTILINE | re.UNICODE
+
+    tokens = {
+        'root': [
+            (r'\n', Text),
+            (r'\s+', Text),
+            (r'#(.*?)\n', Comment.Single),
+            (r'@(.*?)\n', Comment.Single),
+            (r'(\"|\')(\w|\d|[^\"\']|(\\\")|(\\\'))+?(\"|\')', Literal),
+            (r'([A-Z_]+)', Keyword),
+            (r'([a-z_]+)', Name),
+            (r'((->)|[!?+*|])', Operator),
+            (r'[\(\),\{\}]', Punctuation)
+        ]
+    }
+
---
+++ tests/test_rita.py
@@ -0,0 +1,35 @@
+import pytest
+
+from pygments.lexers import RitaLexer
+from pygments.token import Keyword, Literal, Punctuation, Text, Operator
+
+
+@pytest.fixture(scope='session')
+def lexer():
+    yield RitaLexer()
+
+def test_2_macros(lexer):
+    text = '{WORD("Hello"), WORD("world")}->MARK("GREETING")'
+    expected = [
+        (Punctuation, '{'),
+        (Keyword, 'WORD'),
+        (Punctuation, '('),
+        (Literal, '"Hello"'),
+        (Punctuation, ')'),
+        (Punctuation, ','),
+        (Text, ' '),
+        (Keyword, 'WORD'),
+        (Punctuation, '('),
+        (Literal, '"world"'),
+        (Punctuation, ')'),
+        (Punctuation, '}'),
+        (Operator, '->'),
+        (Keyword, 'MARK'),
+        (Punctuation, '('),
+        (Literal, '"GREETING"'),
+        (Punctuation, ')'),
+        (Text, '\n')
+
+    ]
+    assert expected == list(lexer.get_tokens(text))
+
