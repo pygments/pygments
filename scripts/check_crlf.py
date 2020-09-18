6a1821e695b63b7c483d647fb3527bd08e961725
Matthäus G. Chajdas <dev@anteru.net>
2020-09-18 18:23:59 +0200

    Add a check for CR/LF in files.
    This can occur when checking out things on Windows, and it breaks the
    tarball. This adds a script to check for the presence of CR/LF which
    exits early if anything gets found.

---
+++ scripts/check_crlf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Checker for line endings
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Make sure each Python does not use Windows-style file endings.
+
+    :copyright: Copyright 2006-2020 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+import sys
+import os
+
+if __name__ == '__main__':
+    for directory in sys.argv[1:]:
+        if not os.path.exists(directory):
+            continue
+
+        for root, dirs, files in os.walk(directory):
+            for filename in files:
+                if not filename.endswith('.py'):
+                    continue
+
+                with open(os.path.join(root, filename), 'rb') as f:
+                    if b'\r\n' in f.read():
+                        sys.exit(1)
+    
+    sys.exit(0)
