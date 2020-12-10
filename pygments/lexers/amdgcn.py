32a7172065127ffb17d3d75075560b5ad91787b8
Thomas Symalla <root@SEUCHOMAT.localdomain>
2020-12-10 17:13:41 +0100

    Tried to fix case-sensitive issue.

--- pygments/lexers/amdgcn.py
+++ pygments/lexers/amdgcn.py
@@ -27,18 +27,18 @@ class AMDGCNLexer(RegexLexer):
         'root': [
             (r'\s+', Whitespace),
             (r'[\r\n]+', Text),
-            (r'(([a-zA-Z_0-9])*:([a-zA-Z_0-9])*)', Name.Attribute),
+            (r'(([a-z_0-9])*:([a-z_0-9])*)', Name.Attribute),
             (r'(\[|\]|\(|\)|,|\:|\&)', Text),
             (r'([;#]|//).*?\n', Comment.Single),
-            (r'((s_)?(ds|buffer|flat|image)_[a-zA-Z0-9_]+)', Keyword.Reserved),
+            (r'((s_)?(ds|buffer|flat|image)_[a-z0-9_]+)', Keyword.Reserved),
             (r'(_lo|_hi)', Name.Variable),
             (r'(vmcnt|lgkmcnt|expcnt|vmcnt|lit|unorm|glc)', Name.Attribute),
-            (r'(label_[a-zA-Z0-9]+)', Keyword),
+            (r'(label_[a-z0-9]+)', Keyword),
             (r'(_L[0-9]*)', Name.Variable),
-            (r'(s|v)_[a-zA-Z0-9_]+', Keyword),
+            (r'(s|v)_[a-z0-9_]+', Keyword),
             (r'(v[0-9.]+|vcc|exec|v)', Name.Variable),
             (r's[0-9.]+|s', Name.Variable),
             (r'[0-9]+\.[^0-9]+', Number.Float),
-            (r'(0[xX][a-zA-Z0-9]+)|([0-9]+)', Number.Integer)
+            (r'(0[xX][a-z0-9]+)|([0-9]+)', Number.Integer)
         ]
     }
