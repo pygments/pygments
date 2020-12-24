bd378d0ef57a6bc53e9079c9363a6eab0d54f0dc
Daniel Vilar <danvilar@users.noreply.github.com>
2020-12-24 19:18:30 +0000

    Added dracula theme style (#1636)
    * added dracula theme style
    
    * added dracula style to init
    
    * corrected misspell
    
    * Update __init__.py

--- pygments/styles/__init__.py
+++ pygments/styles/__init__.py
@@ -51,6 +51,7 @@ STYLE_MAP = {
     'stata-light': 'stata_light::StataLightStyle',
     'stata-dark':  'stata_dark::StataDarkStyle',
     'inkpot':      'inkpot::InkPotStyle',
+    'dracula': 'dracula::DraculaStyle'
 }
 
 
