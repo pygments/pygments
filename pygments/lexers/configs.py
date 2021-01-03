a590ac5ea7c00a41e253834306bfa19e38349c0b
Matthäus G. Chajdas <dev@anteru.net>
2021-01-03 18:03:23 +0100

    Bump copyright year.

--- pygments/lexers/configs.py
+++ pygments/lexers/configs.py
@@ -5,7 +5,7 @@
 
     Lexers for configuration file formats.
 
-    :copyright: Copyright 2006-2020 by the Pygments team, see AUTHORS.
+    :copyright: Copyright 2006-2021 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
