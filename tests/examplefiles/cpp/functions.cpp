175ba3ca955d9d6065f2ac65b28879318f595459
amitkummer <amit.kummer@gmail.com>
2021-02-21 13:37:49 +0200

    Add tests for lexing of function names

---
+++ tests/examplefiles/cpp/functions.cpp
@@ -0,0 +1,62 @@
+string contains(const char str);
+string contains(const char str) {}
+string* contains(const char str);
+string* contains(const char str) {}
+string * contains(const char str);
+string * contains(const char str) {}
+string    *    contains(const char str);
+string    *    contains(const char str) {}
+string   *contains(const char str);
+string   *contains(const char str) {}
+string   **contains(const char str);
+string   **contains(const char str) {}
+string** contains(const char str);
+string** contains(const char str) {}
+string  ** contains(const char str);
+string  ** contains(const char str) {}
+string  &  contains(const char str);
+string  &  contains(const char str) {}
+string&   contains(const char str);
+string&   contains(const char str) {}
+string   &contains(const char str);
+string   &contains(const char str) {}
+string   &&contains(const char str);
+string   &&contains(const char str) {}
+string   &&   contains(const char str);
+string   &&   contains(const char str) {}
+string&&   contains(const char str);
+string&&   contains(const char str) {}
+const string contains(const char str);
+const string contains(const char str) {}
+explicit const string contains(const char str);
+explicit const string contains(const char str) {}
+explicit const string contains(const char str) noexcept;
+explicit const string contains(const char str) noexcept {}
+explicit const string contains(const char str) noexcept const;
+explicit const string contains(const char str) noexcept const {}
+
+
+explicit const string contains(const char&str);
+explicit const string contains(const char&str) {}
+explicit const string contains(const char&   str);
+explicit const string contains(const char&   str) {}
+explicit const string contains(const char   str);
+explicit const string contains(const char   str) {}
+explicit const string contains(const char&&   str);
+explicit const string contains(const char&&   str) {}
+explicit const string contains(const char&&   str);
+explicit const string contains(const char&&   str) {}
+explicit const string contains(const char &&str);
+explicit const string contains(const char &&str) {}
+explicit const string contains(const char *****  str);
+explicit const string contains(const char *****  str) {}
+explicit const string contains(const char *****str);
+explicit const string contains(const char *****str) {}
+explicit const string contains(const char***** str);
+explicit const string contains(const char***** str) {}
+explicit const string contains(const char *str);
+explicit const string contains(const char *str) {}
+explicit const string contains(const char* str);
+explicit const string contains(const char* str) {}
+explicit const string contains(const char * str);
+explicit const string contains(const char * str) {}
