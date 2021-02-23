37e31b972dfe8d24747e5c48e13a189e70b08cb4
amitkummer <amit.kummer@gmail.com>
2021-02-23 13:11:03 +0200

    Add tests for namespaced names in functions

--- tests/examplefiles/cpp/functions.cpp
+++ tests/examplefiles/cpp/functions.cpp
@@ -1,3 +1,4 @@
+using std::numerical;
 string contains(const char str);
 string contains(const char str) {}
 string* contains(const char str);
@@ -60,3 +61,27 @@ explicit const string contains(const char* str);
 explicit const string contains(const char* str) {}
 explicit const string contains(const char * str);
 explicit const string contains(const char * str) {}
+
+// Names with namespaces
+
+string Type::contains(char c) const noexcept;
+string Type::contains(char c) const noexcept {}
+std::string contains(char c) const noexcept;
+std::string contains(char c) const noexcept {}
+std::string contains(std::vector<char> chars) const noexcept;
+std::string contains(std::vector<char> chars) const noexcept {}
+std::string std::vector::contains(std::vector<char> chars) const;
+std::string std::vector::contains(std::vector<char> chars) const  {}
+const inline explicit std::string std::vector::contains(std::vector<char> chars) const  {}
+const inline explicit std::string std::vector::contains(std::vector<char> chars) const;
+
+// inside classes
+
+class raz {
+    const virtual std::string contains(const std::string str);
+}
+
+// Make sure these are not functions:
+else if(flag && func_call()) {}
+new T();
+const operator int() const {} // so int is lexed as type and not function.name
