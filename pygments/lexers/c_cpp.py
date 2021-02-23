fa6d98d55a55cbc6f1b7008dc4431b45ea13b0df
amitkummer <amit.kummer@gmail.com>
2021-02-23 13:34:31 +0200

    Add support for namespaces in namespace declarations
    Add a namespace state that is entered each time the namespace keyword
    is matched and lexes all name matches as namespaces.
    
    Cases this approach doesn't cover:
    * Namespaces in using declarations.
    * Namespaces that prefix names in random code.
    Unfortunately, in both of these cases the names before and after '::'
    are not always namespaces.

--- pygments/lexers/c_cpp.py
+++ pygments/lexers/c_cpp.py
@@ -339,7 +339,7 @@ class CppLexer(CFamilyLexer):
             (r'(class|concept|typename)(\s+)', bygroups(Keyword, Text), 'classname'),
             (words((
                 'catch', 'const_cast', 'delete', 'dynamic_cast', 'explicit',
-                'export', 'friend', 'mutable', 'namespace', 'new', 'operator',
+                'export', 'friend', 'mutable', 'new', 'operator',
                 'private', 'protected', 'public', 'reinterpret_cast', 'class',
                 'restrict', 'static_cast', 'template', 'this', 'throw', 'throws',
                 'try', 'typeid', 'using', 'virtual', 'constexpr', 'nullptr', 'concept',
@@ -347,12 +347,19 @@ class CppLexer(CFamilyLexer):
                 'co_await', 'co_return', 'co_yield', 'requires', 'import', 'module',
                 'typename'),
                suffix=r'\b'), Keyword),
+            (r'namespace\b', Keyword, 'namespace'),
             (r'(enum)(\s+)', bygroups(Keyword, Text), 'enumname'),
             inherit
         ],
         'types': [
             (r'char(16_t|32_t|8_t)\b', Keyword.Type),
             inherit
+        ],
+        'namespace': [
+            (r'[;{]', Punctuation, ('#pop', 'root')),
+            (r'inline', Keyword),
+            (CFamilyLexer._ident, Name.Namespace),
+            include('statement')
         ]
     }
 
