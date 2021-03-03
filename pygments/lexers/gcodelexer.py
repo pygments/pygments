25ff11337b173be60e6dadef853c5238c3a67861
Averter <averter@users.noreply.github.com>
2021-03-03 11:53:06 +0000

    Added requested changes except words function

--- pygments/lexers/gcodelexer.py
+++ pygments/lexers/gcodelexer.py
@@ -1,9 +1,23 @@
-from pygments.lexer import RegexLexer, bygroups
+# -*- coding: utf-8 -*-
+"""
+    pygments.lexers.gcodelexer.py
+    ~~~~~~~~~~~~~~~~~~~~~~
+    Lexers for gcode Language.
+    :copyright: Copyright 2006-2017 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+from pygments.lexer import RegexLexer, bygroups, words
 from pygments.token import *
 
 __all__ = ['gcodeLexer']
 
 class gcodeLexer(RegexLexer):
+    """
+    For gcode source code.
+    
+    .. versionadded:: 2.80
+    """
     name = 'g-code'
     aliases = ['gcode']
     filenames = ['*.gcode']
