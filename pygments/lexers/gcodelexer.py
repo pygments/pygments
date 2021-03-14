b872095ed5c7c71ae119bb4e04bdf70689cc2336
Averter <averter@users.noreply.github.com>
2021-03-14 08:07:43 +0000

    Add apdl and gcode lexers (#1714)
    * Add apdl and gcode lexers
    
    * Added requested changes except words function
    
    * Added requested changes except words function
    
    * Added words function to handle lists of keywords
    
    * Add two testing example files
    
    * Delete two_dimensional_truss.ans
    
    * Delete coupled_example.ans
    
    * Create example1apdl.ans
    
    * Create example2apdl.ans
    
    * General cleanup of apdlexer and creation of mapfiles
    
    * Escaped backslashes in string literals on apdlexer
    
    * Removed backslashes. None was necessary in the first place
    
    * Added apdl aliase and a * based command in the test file
    
    * Forgot to create the map files
    
    * Tested version. Also removed an unused function from apdlexer
    
    * Added examples for gcode and golden output
    
    * Trimming of gcode files to a few dozens of lines
    
    * Removed duplicates from elafunb

---
+++ pygments/lexers/gcodelexer.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+    pygments.lexers.gcodelexer.py
+    ~~~~~~~~~~~~~~~~~~~~~~
+    Lexers for gcode Language.
+    :copyright: Copyright 2006-2017 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+from pygments.lexer import RegexLexer, bygroups, words
+from pygments.token import *
+
+__all__ = ['gcodeLexer']
+
+class gcodeLexer(RegexLexer):
+    """
+    For gcode source code.
+    
+    .. versionadded:: 2.80
+    """
+    name = 'g-code'
+    aliases = ['gcode']
+    filenames = ['*.gcode']
+    
+    tokens = {
+        'root': [
+            (r';.*\n', Comment),
+            (r'^[gmGM]\d{1,4}\s',Name.Builtin), # M or G commands
+            (r'([^gGmM])([+-]?\d*[.]?\d+)', bygroups(Keyword,Number)),
+            (r'\s', Text.Whitespace),
+            (r'.*\n', Text),
+        ]
+    }
+    
