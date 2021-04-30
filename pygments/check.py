0eca0a99f0f469603f1fc396c6d2cc4f4fedab2b
fusumwan <79878698+fusumwan@users.noreply.github.com>
2021-04-30 16:25:34 +0930

    Update check.py
    Update color contrast checking function

--- pygments/check.py
+++ pygments/check.py
@@ -25,3 +25,15 @@ def valid_range(rgb):
         if (not 0.0 <= r <= 1.0) or (not 0.0 <= g <= 1.0) or (not 0.0 <= b <= 1.0):
             raise ValueError("Invalid range (0.0 - 1.0)")
 
+def passes_contrast_AA(large=False,contrast):
+    if large:
+        return contrast >= 3.0
+    else:
+        return contrast >= 4.5
+
+
+def passes_contrast_AAA(large=False,contrast):
+    if large:
+        return contrast >= 4.5
+    else:
+        return contrast >= 7.0
