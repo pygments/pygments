9a37d64882bd0b8e860fbe434522d657c3c16d02
Hongyuan Yan <54675432+kirito330824@users.noreply.github.com>
2021-05-05 16:32:13 +0930

    updata bdd.lexer

--- pygments/lexers/BDD.py
+++ pygments/lexers/BDD.py
@@ -1,29 +1,58 @@
+"""
+    pygments.lexers.bdd
+    ~~~~~~~~~~~~~~~~~~~~~
+    Lexers for BDD language.
+    :copyright: Copyright 2006-2021 by group 4 of SPI project of TUoA.
+    :license: BSD, see LICENSE for details.
+"""
+
+import re
+
 from pygments.lexer import RegexLexer
 from pygments.token import Text, Keyword, Punctuation
 
+__all__ = ['BddLexer']
 
-__all__ = ['BDDLexer']
 
-class BDDLexer(RegexLexer):
+class BddLexer(RegexLexer):
+
     name = 'BDD'
     aliases = ['bdd']
     filenames = ['*.feature']
 
+    keywords = [
+        'Given'
+        'When',  
+        'Then', 
+        'Add',
+        'Feature', 
+        'Scenario', 
+        'Scenario Outline', 
+        'Background',
+        'Examples',
+        'But',       
+    ]
+
+    Punctuation = [
+        '<',
+        '>',
+        ':',
+        '|',
+
+    ]
+
     tokens = {
         'root': [
+            # Text /get the Text token 
             (r' .*\n', Text),
-            (r'\|.*\n', Punctuation),
-            : ""
-     
-            #keywords
-            (words((
-                'When', 'And', 'Then', 'Given', 'Scenario', 'Background', 'Feature',
-              ), prefix=r'\b'),
-            Keyword),
-
-            # (words(('True', 'False', 'None'), suffix=r'\b'), 
-            # Keyword),
-        
+            (r'\|.*\|',keyword),
+            (r'<.*>',keyword),
+            # Keywords /get the Keyword token 
+            (words(keywords, suffix=r'(?=[^\w-])'), Keyword),
+            #Punctuation /get the Punctuation 
+            #(r'[\{\};\+]+', Punctuation),
+            (words(Punctuation), Punctuation),
+            (r' .*\n', Text),
         ],
        
     }
