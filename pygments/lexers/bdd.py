0cc201dc6c03333bc72d1864962bb8d2fb57be35
Xuan <jypercent@gmail.com>
2021-05-12 21:30:26 +0930

    fix the quote recognition

--- pygments/lexers/bdd.py
+++ pygments/lexers/bdd.py
@@ -2,64 +2,127 @@
     pygments.lexers.bdd
     ~~~~~~~~~~~~~~~~~~~~~
 
-    Lexers for Bdd (Behaviour Driven Development).
+    Lexers for BDD language.
 
     :copyright: Copyright 2006-2021 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
-import re
-
-from pygments.lexer import RegexLexer, words
-from pygments.token import Token, Text, Keyword, Punctuation, Number, Comment, Whitespace
+from pygments.lexer import RegexLexer, include, bygroups
+from pygments.token import Comment, Keyword, Name, String, Number, Generic, Text
 
 __all__ = ['BddLexer']
 
 class BddLexer(RegexLexer):
-    """
-    Lexer for Bdd.
-
-    grammar: https://www.agilealliance.org/glossary/bdd
-    """
-
-    name = 'Bdd'
+    name = 'BDD'
     aliases = ['bdd']
     filenames = ['*.feature']
-    mimetypes = []
-    
-    structure_keywords = (
-        words(('Feature', 'Scenario', 'Given', 'When', 'Then', 
-               'Examples', 'And', 'But', 'Scenario Outline', 'Background'), suffix=r'\b'),
-        Keyword)
+    mimetypes = ['text/x-bdd']
+
+    feature_keywords = '^()(:)(.*)$'
+    feature_element_keywords = '^(\\s*)()(:)(.*)$'
+    examples_keywords = '^(\\s*)()(:)(.*)$'
+    step_keywords = '^(\\s*)(Given|When|Then|Add|And|Feature|Scenario Outline|Scenario|Background|Examples|But|\\* )'
 
     tokens = {
+        'comments': [
+            (r'^\s*#.*$', Comment),
+        ],
+        'feature_elements': [
+            (step_keywords, Keyword, "step_content_stack"),
+            include('comments'),
+            (r"(\s|.)", Name.Function),
+        ],
+        'feature_elements_on_stack': [
+            (step_keywords, Keyword, "#pop:2"),
+            include('comments'),
+            (r"(\s|.)", Name.Function),
+        ],
+        'examples_table': [
+            (r"\s+\|", Keyword, 'examples_table_header'),
+            include('comments'),
+            (r"(\s|.)", Name.Function),
+        ],
+        'examples_table_header': [
+            (r"\s+\|\s*$", Keyword, "#pop:2"),
+            include('comments'),
+            (r"\\\|", Name.Variable),
+            (r"\s*\|", Keyword),
+            (r"[^|]", Name.Variable),
+        ],
+        'scenario_sections_on_stack': [
+            (feature_element_keywords,
+             bygroups(Name.Function, Keyword, Keyword, Name.Function),
+             "feature_elements_on_stack"),
+        ],
+        'narrative': [
+            include('scenario_sections_on_stack'),
+            include('comments'),
+            (r"(\s|.)", Name.Function),
+        ],
+        'table_vars': [
+            (r'(<|>)', Keyword.Type),
+            (r'(\|)', Keyword.Type),
+            (r'(:)', Keyword.Type),
+
+            (r'((?<=\<)[^\\>]+(?=\>))', Name.Variable),
+            #(r'(["][\w\s]+["])', Name.Exception),
+            #Quotes recognition
+            (r'"([^\"]*)"', Name.Exception),
+        ],
+        'numbers': [
+            (r'(\d+\.?\d*|\d*\.\d+)([eE][+-]?[0-9]+)?', String),
+        ],
+        'string': [
+            include('table_vars'),
+            (r'(\s|.)', String),
+        ],
+        'py_string': [
+            (r'"""', Keyword, "#pop"),
+            include('string'),
+        ],
+        'step_content_root': [
+            (r"$", Keyword, "#pop"),
+            include('step_content'),
+        ],
+        'step_content_stack': [
+            (r"$", Keyword, "#pop:2"),
+            include('step_content'),
+        ],
+        'step_content': [
+            include('table_vars'),
+            include('numbers'),
+            include('comments'),
+            (r'(\s|.)', Name.Function),
+        ],
+        'table_content': [
+            (r"\s+\|\s*$", Keyword, "#pop"),
+            include('comments'),
+            (r"\\\|", String),
+            (r"\s*\|", Keyword),
+            include('string'),
+        ],
+        'double_string': [
+            (r'"', Name.Function, "#pop"),
+            include('string'),
+        ],
         'root': [
-            # Whitespace
-            (r'\s+', Whitespace), 
-            (r'\n', Whitespace),
-            
-            #Punctuation
-            #(r'[:\|<>"{}()\[\]]', Punctuation),
-            (r'<.*>|".*"|\||:', Token.Punctuation),
-
-            # Comments
-            (r'//.*', Comment.Single),
-
-            # Keywords
-            structure_keywords,
-
-            # Float numbers
-            (r'0x[0-9a-fA-F]+\.[0-9a-fA-F]+([pP][\-+]?[0-9a-fA-F]+)?', Number.Float),
-            (r'0x[0-9a-fA-F]+\.?[pP][\-+]?[0-9a-fA-F]+', Number.Float),
-            (r'[0-9]+\.[0-9]+([eE][-+]?[0-9]+)?', Number.Float),
-            (r'[-+]?[0-9]*\.?[0-9]+', Number.Float),
-
-            # Integer numbers
-            (r'0b[01]+', Number.Bin),
-            (r'0o[0-7]+', Number.Oct),
-            (r'0x[0-9a-fA-F]+', Number.Hex),
-            (r'[0-9]+', Number.Integer),
-
-            #(r'.', Text), 
-        ],    
+            (r'\n', Name.Function),
+            include('comments'),
+            (r'"""', Keyword, "py_string"),
+            include('table_vars'),
+            include('numbers'),
+            (r'(\s*)(@[^@\r\n\t\f\v]+)', bygroups(Name.Function, Name.Label)),
+            (step_keywords, bygroups(Name.Function, Keyword),
+             'step_content_root'),
+            (feature_keywords, bygroups(Keyword, Keyword, Name.Function),
+             'narrative'),
+            (feature_element_keywords,
+             bygroups(Name.Function, Keyword, Keyword, Name.Function),
+             'feature_elements'),
+            (examples_keywords,
+             bygroups(Name.Function, Keyword, Keyword, Name.Function),
+             'examples_table'),
+            (r'(\s|.)', Name.Function),
+        ]
     }
