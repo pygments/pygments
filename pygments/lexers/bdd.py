d726e3669cf0cd27f9f55d779bf039882cb38d5e
Xuan <jypercent@gmail.com>
2021-05-14 02:46:23 +0930

    update the bdd lexer

--- pygments/lexers/bdd.py
+++ pygments/lexers/bdd.py
@@ -2,18 +2,26 @@
     pygments.lexers.bdd
     ~~~~~~~~~~~~~~~~~~~~~
 
-    Lexers for BDD language.
+    Lexer for BDD(Behavior-driven development). 
+    More information: https://en.wikipedia.org/wiki/Behavior-driven_development
 
     :copyright: Copyright 2006-2021 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
 from pygments.lexer import RegexLexer, include, bygroups
-from pygments.token import Comment, Keyword, Name, String, Number, Generic, Text
+from pygments.token import Comment, Keyword, Name, String, Number, Text, Punctuation
 
 __all__ = ['BddLexer']
 
 class BddLexer(RegexLexer):
+    """
+    Lexer for BDD(Behavior-driven development), which highlights not only keywords, 
+    but also comments, punctuations, strings, numbers, and variables.
+
+    .. versionadded:: 2.10
+    """
+
     name = 'Bdd'
     aliases = ['bdd']
     filenames = ['*.feature']
@@ -23,7 +31,7 @@ class BddLexer(RegexLexer):
     feature_keywords = '^()(:)(.*)$'
     feature_element_keywords = '^(\\s*)()(:)(.*)$'
     examples_keywords = '^(\\s*)()(:)(.*)$'
-    step_keywords = '^(\\s*)(Given|When|Then|Add|And|Feature|Scenario Outline|Scenario|Background|Examples|But|\\* )'
+    step_keywords = '^(\\s*)(Given|When|Then|Add|And|Feature|Scenario Outline|Scenario|Background|Examples|But)'
 
     tokens = {
         'comments': [
@@ -32,48 +40,48 @@ class BddLexer(RegexLexer):
         'feature_elements': [
             (step_keywords, Keyword, "step_content_stack"),
             include('comments'),
-            (r"(\s|.)", Name.Function),
+            (r"(\s)", Text),
         ],
         'feature_elements_on_stack': [
             (step_keywords, Keyword, "#pop:2"),
             include('comments'),
-            (r"(\s|.)", Name.Function),
+            (r"(\s)", Text),
         ],
         'examples_table': [
             (r"\s+\|", Keyword, 'examples_table_header'),
             include('comments'),
-            (r"(\s|.)", Name.Function),
+            (r"(\s)", Text),
         ],
-        'examples_table_header': [ # special example tables
+        'examples_table_header': [
             (r"\s+\|\s*$", Keyword, "#pop:2"),
             include('comments'),
-            (r"\\\|", Name.Variable),
-            (r"\s*\|", Keyword),
+            (r"\\\|", Punctuation),
+            (r"\s*\|", Punctuation),
             (r"[^|]", Name.Variable),
         ],
         'scenario_sections_on_stack': [
             (feature_element_keywords,
-             bygroups(Name.Function, Keyword, Keyword, Name.Function),
+             bygroups(Text, Keyword, Keyword, Text),
              "feature_elements_on_stack"),
         ],
-        'narrative': [ # narrative text
+        'narrative': [
             include('scenario_sections_on_stack'),
             include('comments'),
-            (r"(\s|.)", Name.Function),
+            (r"(\s|.)", Text),
         ],
         'table_vars': [
-            (r'(<|>)', Keyword.Type),
-            (r'(\|)', Keyword.Type),
-            (r'(:)', Keyword.Type),
+            (r'(<|>)', Punctuation),
+            (r'(\|)', Punctuation),
+            (r'(:)', Punctuation),
             (r'((?<=\<)[^\\>]+(?=\>))', Name.Variable),
-            (r'"([^\"]*)"', Name.Exception),
+            (r'"([^\"]*)"', String),
         ],
         'numbers': [
-            (r'(\d+\.?\d*|\d*\.\d+)([eE][+-]?[0-9]+)?', String),
+            (r'(\d+\.?\d*|\d*\.\d+)([eE][+-]?[0-9]+)?', Number),
         ],
         'string': [
             include('table_vars'),
-            (r'(\s|.)', String),
+            (r'(\s|.)', Text),
         ],
         'py_string': [
             (r'"""', Keyword, "#pop"),
@@ -91,36 +99,36 @@ class BddLexer(RegexLexer):
             include('table_vars'),
             include('numbers'),
             include('comments'),
-            (r'(\s|.)', Name.Function),
+            (r'(\s|.)', Text),
         ],
         'table_content': [
             (r"\s+\|\s*$", Keyword, "#pop"),
             include('comments'),
-            (r"\\\|", String),
-            (r"\s*\|", Keyword),
+            (r"\\\|", Punctuation),
+            (r"\s*\|", Punctuation),
             include('string'),
         ],
         'double_string': [
-            (r'"', Name.Function, "#pop"),
+            (r'"', Text, "#pop"),
             include('string'),
         ],
         'root': [
-            (r'\n', Name.Function),
+            (r'\n', Text),
             include('comments'),
             (r'"""', Keyword, "py_string"),
             include('table_vars'),
             include('numbers'),
-            (r'(\s*)(@[^@\r\n\t\f\v]+)', bygroups(Name.Function, Name.Label)), # start with @
-            (step_keywords, bygroups(Name.Function, Keyword),
+            (r'(\s*)(@[^@\r\n\t\f\v]+)', bygroups(Text, Name.Label)),
+            (step_keywords, bygroups(Text, Keyword),
              'step_content_root'),
-            (feature_keywords, bygroups(Keyword, Keyword, Name.Function),
+            (feature_keywords, bygroups(Keyword, Text),
              'narrative'),
             (feature_element_keywords,
-             bygroups(Name.Function, Keyword, Keyword, Name.Function),
+             bygroups(Text, Keyword),
              'feature_elements'),
             (examples_keywords,
-             bygroups(Name.Function, Keyword, Keyword, Name.Function),
+             bygroups(Text, Keyword),
              'examples_table'),
-            (r'(\s|.)', Name.Function),
+            (r'(\s|.)', Text),
         ]
     }
--- tests/examplefiles/bdd/example.feature
+++ tests/examplefiles/bdd/example.feature
@@ -29,13 +29,3 @@ Feature: Serve coffee
       | start | eat | left |
       |  12   |  5  |  7   |
       |  20   |  5  |  15  |
-
-  Scenario Outline: eating
-    Given there are $start cucumbers
-    When I eat $eat cucumbers
-    Then I should have $left cucumbers
-
-    Examples:
-      | start | eat | left |
-      |  12   |  5  |  7   |
-      |  20   |  5  |  15  |
