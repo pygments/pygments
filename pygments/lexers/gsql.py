18be30d7babaa18f7c489e93d5d433f6037bcc03
Dan B <daniel.barkus@gmail.com>
2021-05-19 09:51:34 -0400

    encased keywords in 'words' function added link to language reference

--- pygments/lexers/gsql.py
+++ pygments/lexers/gsql.py
@@ -8,6 +8,12 @@ from pygments.token import Keyword, Punctuation, Comment, Operator, Name,\
 __all__ = ["GSQLLexer"]
 
 class GSQLLexer(RegexLexer):
+
+    """
+    For `GSQL <https://docs.tigergraph.com/dev/gsql-ref>`_ queries (version 3.x).
+    .. versionadded:: 2.10
+    """
+
     name = 'GSQL'
     aliases = ['gsql']
     filenames = ['*.gsql']
@@ -31,17 +37,18 @@ class GSQLLexer(RegexLexer):
             (r'.*\/\*\s*.*\s*\*\/', Comment.Multiline),
         ],
         'keywords': [
-            (r'(ACCUM|AND|ANY|API|AS|ASC|AVG|BAG|BATCH|BETWEEN|BOOL|BOTH|'
-             r'BREAK|BY|CASE|CATCH|COALESCE|COMPRESS|CONTINUE|COUNT|'
-             r'CREATE|DATETIME|DATETIME_ADD|DATETIME_SUB|DELETE|DESC|DISTRIBUTED|DO|'
-             r'DOUBLE|EDGE|ELSE|END|ESCAPE|EXCEPTION|FALSE|FILE|FILTER|FLOAT|FOREACH|FOR|'
-             r'FROM|GRAPH|GROUP|GSQL_INT_MAX|GSQL_INT_MIN|GSQL_UINT_MAX|HAVING|IF|'
-             r'IN|INSERT|INT|INTERPRET|INTERSECT|INTERVAL|INTO|IS|ISEMPTY|JSONARRAY|JSONOBJECT|LASTHOP|'
-             r'LEADING|LIKE|LIMIT|LIST|LOAD_ACCUM|LOG|MAP|MATCH|MAX|MIN|MINUS|NOT|'
-             r'NOW|NULL|OFFSET|OR|ORDER|PATH|PER|PINNED|POST_ACCUM|POST-ACCUM|PRIMARY_ID|PRINT|'
-             r'QUERY|RAISE|RANGE|REPLACE|RESET_COLLECTION_ACCUM|RETURN|RETURNS|RUN|SAMPLE|SELECT|SELECT_VERTEX|'
-             r'SET|SRC|STATIC|STRING|SUM|SYNTAX|TARGET|TAGSTGT|THEN|TO|TO_CSV|TO_DATETIME|TRAILING|TRIM|TRUE|'
-             r'TRY|TUPLE|TYPEDEF|UINT|UNION|UPDATE|VALUES|VERTEX|WHEN|WHERE|WHILE|WITH)\b', Keyword),
+            (words((
+             'ACCUM', 'AND', 'ANY', 'API', 'AS', 'ASC', 'AVG', 'BAG', 'BATCH', 'BETWEEN', 'BOOL', 'BOTH', 
+             'BREAK', 'BY', 'CASE', 'CATCH', 'COALESCE', 'COMPRESS', 'CONTINUE', 'COUNT', 
+             'CREATE', 'DATETIME', 'DATETIME_ADD', 'DATETIME_SUB', 'DELETE', 'DESC', 'DISTRIBUTED', 'DO', 
+             'DOUBLE', 'EDGE', 'ELSE', 'END', 'ESCAPE', 'EXCEPTION', 'FALSE', 'FILE', 'FILTER', 'FLOAT', 'FOREACH', 'FOR', 
+             'FROM', 'GRAPH', 'GROUP', 'GSQL_INT_MAX', 'GSQL_INT_MIN', 'GSQL_UINT_MAX', 'HAVING', 'IF', 
+             'IN', 'INSERT', 'INT', 'INTERPRET', 'INTERSECT', 'INTERVAL', 'INTO', 'IS', 'ISEMPTY', 'JSONARRAY', 'JSONOBJECT', 'LASTHOP', 
+             'LEADING', 'LIKE', 'LIMIT', 'LIST', 'LOAD_ACCUM', 'LOG', 'MAP', 'MATCH', 'MAX', 'MIN', 'MINUS', 'NOT', 
+             'NOW', 'NULL', 'OFFSET', 'OR', 'ORDER', 'PATH', 'PER', 'PINNED', 'POST_ACCUM', 'POST-ACCUM', 'PRIMARY_ID', 'PRINT', 
+             'QUERY', 'RAISE', 'RANGE', 'REPLACE', 'RESET_COLLECTION_ACCUM', 'RETURN', 'RETURNS', 'RUN', 'SAMPLE', 'SELECT', 'SELECT_VERTEX', 
+             'SET', 'SRC', 'STATIC', 'STRING', 'SUM', 'SYNTAX', 'TARGET', 'TAGSTGT', 'THEN', 'TO', 'TO_CSV', 'TO_DATETIME', 'TRAILING', 'TRIM', 'TRUE', 
+             'TRY', 'TUPLE', 'TYPEDEF', 'UINT', 'UNION', 'UPDATE', 'VALUES', 'VERTEX', 'WHEN', 'WHERE', 'WHILE', 'WITH'), prefix=r'(?<!\.)', suffix=r'\b'), Name.Builtin)
         ],
         'clauses': [
             (r'(accum|having|limit|order|postAccum|sample|where)\b', Keyword),
