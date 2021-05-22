283ea5c30897125b21d90b2a40b2c52a3ac41182
Kai <7957852+kaivol@users.noreply.github.com>
2021-05-22 15:01:39 +0200

    Fixed Rust attributes

--- pygments/lexers/rust.py
+++ pygments/lexers/rust.py
@@ -214,15 +214,21 @@ class RustLexer(RegexLexer):
             (r'"', String, 'string'),
             (r'\[', Comment.Preproc, 'attribute['),
             (r'\(', Comment.Preproc, 'attribute('),
+            (r'\{', Comment.Preproc, 'attribute{'),
         ],
         'attribute[': [
             include('attribute_common'),
             (r'\];?', Comment.Preproc, '#pop'),
-            (r'[^"\]]+', Comment.Preproc),
+            (r'[^"\]\[({]+', Comment.Preproc),
         ],
         'attribute(': [
             include('attribute_common'),
             (r'\);?', Comment.Preproc, '#pop'),
-            (r'[^")]+', Comment.Preproc),
+            (r'[^")\[({]+', Comment.Preproc),
+        ],
+        'attribute{': [
+            include('attribute_common'),
+            (r'};?', Comment.Preproc, '#pop'),
+            (r'[^"}\[({]+', Comment.Preproc),
         ],
     }
