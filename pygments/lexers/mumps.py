11d915f98c5236c2de91dfbfe4b127bab6088309
Avery <saint_erk@tutanota.com>
2021-07-07 18:16:50 -0500

    Add morphing quit and function tests

--- pygments/lexers/mumps.py
+++ pygments/lexers/mumps.py
@@ -6,16 +6,16 @@
     Lexers for MUMPS
 
     :copyright: Copyright 2020 by the Pygments team, see AUTHORS.
-    :licens: BSD, see LICENSE for details.
+    :license: BSD, see LICENSE for details.
 """
 
 import re
-from pygments.lexer import RegexLexer, words, bygroups, default, include
-from pygments.token import Whitespace, Keyword
+from pygments.lexer import ExtendedRegexLexer, words, bygroups, default, include
+from pygments.token import Whitespace, Keyword, Operator, Number, Name, Punctuation, Comment
 
 __all__ = ['MumpsLexer']
 
-class MumpsLexer(RegexLexer):
+class MumpsLexer(ExtendedRegexLexer):
     """
     For MUMPS source code.
     
@@ -30,12 +30,121 @@ class MumpsLexer(RegexLexer):
     # For example, YottaDB would have the source file for "dmex" in "dmex.m"
     flags = re.IGNORECASE
 
+    name_re = '[A-Za-z]+'
+    binaryop_re = '<'
+    unaryop_re = '[-+]'
+
+    ## Regular expressions
     tokens = {
         'root': [
-            (' ', Whitespace, 'linebody'),
-        ],
+            ###
+            # 6 - Routine
+            ###
+            default('routinebody')
+            ],
+        # 6.1 - Routine head 'routinehead'
+        'name': [
+            (name_re, Name, '#pop'),
+            ],
+        # 6.2 - Routine body 'routinebody'
+        'routinebody': [ 
+            # Not handled: EOR, special characters
+            default('line'),           
+            ],
+        'line': [
+            # 6.2.1 levelline - Label is optional
+            ('(' + name_re + ')( +)', bygroups(Name.Label, Whitespace), ('#pop', 'linebody')), 
+            (' ', Whitespace, ('#pop', 'linebody')),
+            # 6.2.2 formalline
+            ('(' + name_re + ')(\\()', bygroups(Name.Function, Punctuation), 'formallist'),
+            ],
+        # 6.2.2 Formal line 'formalline'
+        'formallist': [
+            ('\\)', Punctuation, '#pop'),
+	    default('l_name'),
+            ],
+        # list of 'name' continuation
+        'l_name': [
+	    #('(' + name_re + ')(,)', bygroups(Name.Variable, Punctuation)),
+	    ( name_re, Name.Variable, '#pop')
+	    ],
+        # 6.2.3 Label 'label'
+        'label': [
+            (name_re, Name.Label, '#pop'),
+            ],
+        # 6.2.5 - Line body 'linebody'
         'linebody': [
-            ('q', Keyword),
-            ('\n', Whitespace, '#pop'),
-        ],
-    }
+	    (';.*', Comment, '#pop'),
+            default('commands'),
+            ],
+        'commands': [
+            (' +', Whitespace),
+            default('command')
+            ],
+        'cs_commands': [
+            (' ', Whitespace, 'command'),
+            default('#pop'),
+            ],    
+        ###
+        # 7 - Expression 'expr'
+        ###
+        'expr': [
+            default(('#pop', 'exprtail', 'expratom')) 
+            ],
+        # 7.1 - expratom
+        'expratom': [
+            include('glvn'),
+            include('expritem'),
+            ],
+	# 7.1.2 - Variable name 'glvn'
+	'glvn': [
+	    ( name_re, Name.Variable, '#pop'),
+	    ],
+        # 7.1.4 - Expression item 'expritem'
+        'expritem': [
+            #include('strlit'),
+            include('numlit'),
+            #include('exfunc'),
+            #include('exvar'),
+            include('svn'),
+            #include('function'),
+	    ( unaryop_re, Operator, ('#pop', 'expratom')),
+            ],
+        # 7.1.4.2 - Numeric literal 'numlit'
+        'numlit': [
+            ('[0-9]+', Number, '#pop'),  
+            ],
+        # 7.1.4.10 - Intrinsic special variable names 'svn'
+        'svn': [
+            ('\\$QUIT', Keyword, '#pop'),
+            ],
+        # 7.2 - exprtail
+        'exprtail': [
+            # TODO
+	    ( binaryop_re, Operator, 'expratom'),
+            default('#pop')
+            ],
+        ###
+        # 8 - Commands
+        ###
+        # 8.1 - Command general rules
+        # note - we include each 'commandword' here because each has its own sytax
+        'command': [
+            ('quit|q', Keyword, ('#pop', 'quitarg', 'postcond')),
+            ],
+        # 8.1.4 - postcond
+        'postcond': [
+            (':', Operator, 'expr'),
+            default('#pop')  
+            ],
+        # 8.2.16 - QUIT
+        'quitarg': [
+            ('  ', Whitespace, '#pop'),
+            (' ', Whitespace, ('#pop', 'expr_or_indirect')),
+            default('#pop')
+            ],
+        'expr_or_indirect': [
+            ('@', Operator, ('#pop', 'expratom')),
+            include('expr')
+            ],
+        }
--- tests/test_mumps.py
+++ tests/test_mumps.py
@@ -9,7 +9,7 @@
 
 import pytest
 
-from pygments.token import Whitespace, Keyword
+from pygments.token import Whitespace, Keyword, Number, Operator, Text, Name, Punctuation, Comment
 from pygments.lexers import MumpsLexer
 
 @pytest.fixture(scope='module')
@@ -21,7 +21,86 @@ def test_quit_line(lexer):
     tokens = [
         (Whitespace, ' '),
         (Keyword, 'q'),
-        (Whitespace, '\n')
+        (Text, '\n')
     ]
+    assert list(lexer.get_tokens(fragment)) == tokens
+
+def test_quit_value_line(lexer):
+    fragment = ' QUIT 1'
+    tokens = [
+        (Whitespace, ' '),
+        (Keyword, 'QUIT'),
+        (Whitespace, ' '),
+        (Number, '1'),
+        (Text, '\n')
+    ]
+    assert list(lexer.get_tokens(fragment)) == tokens
+
+def test_quit_morph_line(lexer):
+    fragment = ' q:$QUIT 1 Q'
+    tokens = [
+        (Whitespace, ' '),
+        (Keyword, 'q'),
+        (Operator, ':'),
+        (Keyword, '$QUIT'),
+        (Whitespace, ' '),
+        (Number, '1'),
+        (Whitespace, ' '),
+        (Keyword, 'Q'),
+        (Text, '\n')
+    ]
+    assert list(lexer.get_tokens(fragment)) == tokens
+
+def test_stub_subroutine(lexer):
+    fragment = 'stub q'
+    tokens = [
+        (Name.Label, 'stub'),
+        (Whitespace, ' '),
+        (Keyword, 'q'),
+        (Text, '\n')
+        ]
+    assert list(lexer.get_tokens(fragment)) == tokens
+
+def test_simple_function(lexer):
+    fragment = 'true() q 1'
+    tokens = [
+        (Name.Function, 'true'),
+        (Punctuation, '('),
+        (Punctuation, ')'),
+        (Whitespace, ' '),
+        (Keyword, 'q'),
+        (Whitespace, ' '),
+        (Number, '1'),
+        (Text, '\n')
+        ]
+    assert list(lexer.get_tokens(fragment)) == tokens
+
+def test_simple_function(lexer):
+    fragment = 'abs(value) ; Returns the absolute value of a value\n q:value<0 -value\n q +value'
+    tokens = [
+        (Name.Function, 'abs'),
+	(Punctuation, '('),
+	(Name.Variable, 'value'),
+	(Punctuation, ')'),
+	(Whitespace, ' '),
+	(Comment, '; Returns the absolute value of a value'),
+	(Text, '\n'),
+	(Whitespace, ' '),
+	(Keyword, 'q'),
+	(Operator, ':'),
+	(Name.Variable, 'value'),
+	(Operator, '<'),
+	(Number, '0'),
+	(Whitespace, ' '),
+	(Operator, '-'),
+	(Name.Variable, 'value'),
+	(Text, '\n'),
+	(Whitespace, ' '),
+	(Keyword, 'q'),
+	(Whitespace, ' '),
+	(Operator, '+'),
+	(Name.Variable, 'value'),
+	(Text, '\n'),
+	]
     assert list(lexer.get_tokens(fragment)) == tokens
 
