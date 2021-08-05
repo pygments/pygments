3da5506e953f3220f9cf803980c2d172734c6660
Immanuel Washington <code.immanuelqrw@gmail.com>
2021-08-05 03:49:24 -0400

    Updated Smithy Lexer
    * Added Standard file heading with copyright and license
    * Used `words` method for optimization, instead of bare regex
    * Specified whitespace punctuation in root
    * Updated aliases to only contain lowercase names to pass `test_basic_api` tests
    * Updated regexes lightly to fit regexlint rules (removing duplicate characters in group `-`)
    * Fixed regexes with errors in regexlint rules (Escaping brackets, gaps in capture groups)

--- pygments/lexers/smithy.py
+++ pygments/lexers/smithy.py
@@ -1,16 +1,18 @@
-# -*- coding: utf-8 -*-
 """
-Smithy IDL lexer
-~~~~~~~~~~~~~~~~
+    pygments.lexers.smithy
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Lexers for the Smithy IDL.
+    Lexers for the Smithy IDL.
+
+    :copyright: Copyright 2006-2021 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
 """
 
 import re
 
-from pygments.lexer import RegexLexer, default, include, bygroups
-from pygments.token import Text, Comment, Operator, Keyword, Name, String, \
-    Number, Literal
+from pygments.lexer import RegexLexer, bygroups, words
+from pygments.token import Text, Comment, Keyword, Name, String, \
+    Number, Whitespace, Punctuation
 
 __all__ = ['SmithyLexer']
 
@@ -18,14 +20,27 @@ __all__ = ['SmithyLexer']
 class SmithyLexer(RegexLexer):
     """
     For Smithy IDL
+
+    .. versionadded:: 2.10
     """
     name = 'Smithy'
     filenames = ['*.smithy']
-    aliases = ['smithy', 'Smithy']
+    aliases = ['smithy']
 
     flags = re.MULTILINE | re.UNICODE
-    unquoted = r'[A-Z-a-z0-9_\.#$-]+'
-    identifier = r"[A-Z-a-z0-9_\.#$-]+"
+    unquoted = r'[A-Za-z0-9_\.#$-]+'
+    identifier = r"[A-Za-z0-9_\.#$-]+"
+
+    simple_shapes = (
+        'use', 'byte', 'short', 'integer', 'long', 'float', 'document',
+        'double', 'bigInteger', 'bigDecimal', 'boolean', 'blob', 'string',
+        'timestamp',
+    )
+
+    aggregate_shapes = (
+       'apply', 'list', 'map', 'set', 'structure', 'union', 'resource',
+       'operation', 'service', 'trait'
+    )
 
     tokens = {
         'root': [
@@ -33,15 +48,18 @@ class SmithyLexer(RegexLexer):
             (r'//.*$', Comment),
             (r'@[0-9a-zA-Z\.#-]*', Name.Decorator),
             (r'(=)', Name.Decorator),
-            (r'^(\$version)(:)(.+)', bygroups(Keyword.Declaration, Name.Decorator, Name.Class)),
-            (r'^(namespace)(\s+' + identifier + r')\b', bygroups(Keyword.Declaration, Name.Class)),
-            (r'^(use|byte|short|integer|long|float|'
-             r'document|double|bigInteger|bigDecimal|boolean|blob|string|timestamp)(\s+' + identifier + r')\b',
-             bygroups(Keyword.Declaration, Name.Class)),
-            (r'^(apply|list|map|set|structure|union|resource|operation|service|trait)(\s+' + identifier + r')',
-             bygroups(Keyword.Declaration, Name.Class)),
-            (r'^(metadata)(\s+.+)\s*(=)',
-             bygroups(Keyword.Declaration, Name.Class, Name.Decorator)),
+            (r'^(\$version)(:)(.+)',
+                bygroups(Keyword.Declaration, Name.Decorator, Name.Class)),
+            (r'^(namespace)(\s+' + identifier + r')\b',
+                bygroups(Keyword.Declaration, Name.Class)),
+            (words(simple_shapes,
+                   prefix=r'^', suffix=r'(\s+' + identifier + r')\b'),
+                bygroups(Keyword.Declaration, Name.Class)),
+            (words(aggregate_shapes,
+                   prefix=r'^', suffix=r'(\s+' + identifier + r')'),
+                bygroups(Keyword.Declaration, Name.Class)),
+            (r'^(metadata)(\s+.+)(\s*)(=)',
+                bygroups(Keyword.Declaration, Name.Class, Whitespace, Name.Decorator)),
             (r"(true|false|null)", Keyword.Constant),
             (r"(-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?)", Number),
             (identifier + ":", Name.Label),
@@ -50,11 +68,12 @@ class SmithyLexer(RegexLexer):
             (r'\]', Text, "#pop"),
             (r'\(', Text, "#push"),
             (r'\)', Text, "#pop"),
-            (r'{', Text, "#push"),
-            (r'}', Text, "#pop"),
+            (r'\{', Text, "#push"),
+            (r'\}', Text, "#pop"),
             (r'"{3}(\\\\|\n|\\")*"{3}', String.Doc),
             (r'"(\\\\|\n|\\"|[^"])*"', String.Double),
             (r"'(\\\\|\n|\\'|[^'])*'", String.Single),
-            (r'[:,\s]+', Text),
+            (r'[:,]+', Punctuation),
+            (r'\s+', Whitespace),
         ]
     }
