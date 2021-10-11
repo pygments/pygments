f0ba06c4563055def784c96bd551a3d05e5a5422
DanVJ <geral@danvj.pt>
2021-10-11 23:10:38 +0100

    Added highlight and line colours

--- pygments/styles/dracula.py
+++ pygments/styles/dracula.py
@@ -4,24 +4,44 @@
 
     Pygments version of `Dracula` from https://github.com/dracula/dracula-theme.
 
-    :copyright: Copyright 2006-2021 by the Pygments team, see AUTHORS.
-    :license: BSD, see LICENSE for details.
-
     Based on the Dracula Theme for pygments by Chris Bracco.
     See https://github.com/dracula/pygments/tree/fee9ed5613d1086bc01b9d0a5a0e9867a009f571
+
+    :copyright: Copyright 2006-2021 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
 """
 
 from pygments.style import Style
-from pygments.token import Keyword, Name, Comment, String, Error, \
-    Literal, Number, Operator, Other, Punctuation, Text, Generic, \
-    Whitespace
+from pygments.token import (
+    Keyword,
+    Name,
+    Comment,
+    String,
+    Error,
+    Literal,
+    Number,
+    Operator,
+    Other,
+    Punctuation,
+    Text,
+    Generic,
+    Whitespace,
+)
+
 
 class DraculaStyle(Style):
 
-    background_color = "#282a36"
     default_style = ""
+    background_color = "#282a36"
+    highlight_color = "#44475a"
+    line_number_color = "#f1fa8c"
+    line_number_background_color = "#44475a"
+    line_number_special_color = "#50fa7b"
+    line_number_special_background_color = "#6272a4"
 
     styles = {
+        Whitespace: "#f8f8f2",
+
         Comment: "#6272a4",
         Comment.Hashbang: "#6272a4",
         Comment.Multiline: "#6272a4",
@@ -42,7 +62,6 @@ class DraculaStyle(Style):
         Generic.Traceback: "#f8f8f2",
 
         Error: "#f8f8f2",
-
         Keyword: "#ff79c6",
         Keyword.Constant: "#ff79c6",
         Keyword.Declaration: "#8be9fd italic",
@@ -50,10 +69,8 @@ class DraculaStyle(Style):
         Keyword.Pseudo: "#ff79c6",
         Keyword.Reserved: "#ff79c6",
         Keyword.Type: "#8be9fd",
-
         Literal: "#f8f8f2",
         Literal.Date: "#f8f8f2",
-
         Name: "#f8f8f2",
         Name.Attribute: "#50fa7b",
         Name.Builtin: "#8be9fd italic",
@@ -72,22 +89,17 @@ class DraculaStyle(Style):
         Name.Variable.Class: "#8be9fd italic",
         Name.Variable.Global: "#8be9fd italic",
         Name.Variable.Instance: "#8be9fd italic",
-
         Number: "#ffb86c",
         Number.Bin: "#ffb86c",
         Number.Float: "#ffb86c",
         Number.Hex: "#ffb86c",
         Number.Integer: "#ffb86c",
         Number.Integer.Long: "#ffb86c",
-        Number.Oct: "#ffb86c", #ffb86c
-
+        Number.Oct: "#ffb86c",
         Operator: "#ff79c6",
         Operator.Word: "#ff79c6",
-
         Other: "#f8f8f2",
-
         Punctuation: "#f8f8f2",
-
         String: "#bd93f9",
         String.Backtick: "#bd93f9",
         String.Char: "#bd93f9",
@@ -100,8 +112,5 @@ class DraculaStyle(Style):
         String.Regex: "#bd93f9",
         String.Single: "#bd93f9",
         String.Symbol: "#bd93f9",
-
         Text: "#f8f8f2",
-
-        Whitespace: "#f8f8f2"
     }
