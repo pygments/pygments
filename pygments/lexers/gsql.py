830d7ba768f1ebdf27dc87d9dd9c85a8d64f7002
Gregory Grubbs <gregory@dynapse.com>
2021-12-30 06:24:25 -0700

    GSQL lexer syntax changes (#2006)
    * Add square, angle and single underscore to punct; simplify string regex
    
    * Add dollar as operator in LOAD DDL; add pipe as op for resused edge names
    
    * Add to test file to test recent lexer fixes
    
    * Added PR# to new test cases

--- pygments/lexers/gsql.py
+++ pygments/lexers/gsql.py
@@ -71,12 +71,11 @@ class GSQLLexer(RegexLexer):
         'relations': [
             (r'(-\s?)(\(.*\:\w?\))(\s?-)', bygroups(Operator, using(this), Operator)),
             (r'->|<-', Operator),
-            (r'[.*{}]', Punctuation),
+            (r'[.*{}\[\]\<\>\_]', Punctuation),
         ],
         'strings': [
-            (r'"(?:\\[tbnrf\'"\\]|[^\\"])*"', String),
+            (r'"([^"\\]|\\.)*"', String),
             (r'@{1,2}\w+', Name.Variable),
-            (r'(\<\w+)?\<(\w+\>?\,?\s?)+\>+', Name.Constant),
         ],
         'whitespace': [
             (r'\s+', Whitespace),
@@ -86,7 +85,7 @@ class GSQLLexer(RegexLexer):
             (r'(\d+\.\d+|\d+)', Number),
         ],
         'operators': [
-            (r'[^0-9|\/|\-](\-\=|\+\=|\*\=|\\\=|\=|\=\=|\=\=\=|\+|\-|\*|\\|\+\=|\>|\<)[^\>|\/]', Operator),
-            (r'(\(|\)|\,|\;|\=|\-|\+|\*|\/|\>|\<|\:)', Operator),
+            (r'\$|[^0-9|\/|\-](\-\=|\+\=|\*\=|\\\=|\=|\=\=|\=\=\=|\+|\-|\*|\\|\+\=|\>|\<)[^\>|\/]', Operator),
+            (r'(\||\(|\)|\,|\;|\=|\-|\+|\*|\/|\>|\<|\:)', Operator),
         ],
     }
