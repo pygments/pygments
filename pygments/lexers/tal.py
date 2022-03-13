40310f2e06cda5b2b775c86e55f80146b7178fc2
d_m <d_m@plastic-idolatry.com>
2022-03-13 13:47:10 -0400

    Add docstring for TalLexer (including versionadded).

--- pygments/lexers/tal.py
+++ pygments/lexers/tal.py
@@ -18,6 +18,11 @@ from pygments.token import Comment, Keyword, Name, String, Number, Punctuation, 
 __all__ = ['TalLexer']
 
 class TalLexer(RegexLexer):
+    """
+    For `Uxntal <https://wiki.xxiivv.com/site/uxntal.html>`_ source code.
+
+    .. versionadded:: 2.12
+    """
 
     name = 'Tal'
     aliases = ['tal', 'uxntal']
