9ffdf5380f3c599086525013bad88d02ab8ae41d
Avery <saint_erk@tutanota.com>
2022-03-18 18:31:58 -0500

    Add example file for mumps

---
+++ tests/examplefiles/mumps/EXAMPLE.m
@@ -0,0 +1,10 @@
+EXAMPLE
+ ; This is an example routine to test Pygments parsing
+ write !,"Hello, world!"
+ quit
+gcd(a,b) ; Euclid's algorithm for greatest common denominator
+ n R
+ f  d  q:R=0
+ . s R=a#b
+ . i R'=0 s a=b,b=R
+ q b
