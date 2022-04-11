5f35243933cd92c0ff2f2d0a2cad9018328213ee
Ritik Shah <ritik10.shah@gmail.com>
2022-04-10 23:09:50 -0700

    fixed: generic property

--- pygments/lexers/mcfunction.py
+++ pygments/lexers/mcfunction.py
@@ -8,10 +8,11 @@
     :license: BSD, see LICENSE for details.
 """
 
-from pygments.lexer import RegexLexer, default, include, using
-from pygments.token import (Comment, Error, Keyword, Literal, Name, Number,
+from pygments.lexer import RegexLexer, default, include, using, this, bygroups
+from pygments.token import (Comment, Keyword, Literal, Name, Number,
                             Operator, Punctuation, String, Text, Token,
                             Whitespace)
+import re
 
 
 __all__ = ['SNBTLexer', 'MCFunctionLexer']
@@ -34,8 +35,8 @@ class SNBTLexer(RegexLexer):
         "root": [
             # We only look for the open bracket here since square bracket
             #  is only valid in NBT pathing (which is a mcfunction idea).
-            (r"\{", Punctuation.SNBT.Start, "compound"),
-            (r"[^\{]+", Text.TEST),
+            (r"\{", Punctuation, "compound"),
+            (r"[^\{]+", Text),
         ],
 
         "whitespace": [
@@ -106,7 +107,7 @@ class MCFunctionLexer(RegexLexer):
     mimetypes = ["text/mcfunction"]
 
     # Used to denotate the start of a block comment, borrowed from Github's mcfunction
-    _block_comment_prefix = "[>!*$]"
+    _block_comment_prefix = "[>!*]"
 
     tokens = {
         "root": [
@@ -114,9 +115,8 @@ class MCFunctionLexer(RegexLexer):
             include("comments"),
             include("literals"),
             include("whitespace"),
-            include("nbt"),
+            include("property"),
             include("operators"),
-            include("delimiters"),
             include("selectors"),
         ],
 
@@ -124,8 +124,9 @@ class MCFunctionLexer(RegexLexer):
             # The start of a command (either beginning of line OR after the run keyword)
             #  We don't encode a list of keywords since mods, plugins, or even pre-processors
             #  may add new commands, so we have a 'close-enough' regex which catches them.
-            (r"(?=^\s*)[a-z_]+", Name.Builtin),
+            (r"^(\s*)([a-z_]+)", bygroups(Whitespace, Name.Builtin)),
             (r"(?<=run)\s+[a-z_]+", Name.Builtin),
+
             # UUID
             (
                 r"\b[0-9a-fA-F]+(?:-[0-9a-fA-F]+){4}\b",
@@ -134,19 +135,20 @@ class MCFunctionLexer(RegexLexer):
             include("resource-name"),
             # normal command names and scoreboards
             #  there's no way to know the differences unfortuntely
-            (r"[A-Z_a-z][A-Z_a-z0-9]*", Keyword.Constant, "names.maybe-nbt"),
-            (r"[#%$][A-Z_a-z0-9]+", Name.Variable.Magic, "names.fakeplayer"),
-        ],
-        "names.fakeplayer": [(r"[\.A-Z_a-z0-9]+", Name.Variable.Magic), default("#pop")],
-        "names.maybe-nbt": [
-            (r"\[", Punctuation, "nbt.list"),
-            default("#pop"),
+            (r"[A-Za-z_][A-Za-z0-9_.#%$]+", Keyword.Constant),
+            (r"[#%$][A-Za-z0-9_.#%$]+", Name.Variable.Magic),
         ],
 
         "resource-name": [
             (
                 # resource names have to be lowercase
                 r"#?[a-z_][a-z_\.\-]*\:[a-z0-9_\.\-\/]+",
+                Name.Function,
+            ),
+            (
+                # similar to above except optional `:``
+                #  a `/` must be present "somewhere"
+                r"#?[a-z0-9_\.\-]+\/[a-z0-9_\.\-\/]+",
                 Name.Function,
             )
         ],
@@ -157,7 +159,7 @@ class MCFunctionLexer(RegexLexer):
 
         "comments": [
             (
-                rf"^\s*(#\s*{_block_comment_prefix})",
+                rf"^\s*(#{_block_comment_prefix})",
                 Comment.Multiline,
                 (
                     "comments.block",
@@ -167,34 +169,43 @@ class MCFunctionLexer(RegexLexer):
             (r"#.*$", Comment.Single),
         ],
         "comments.block": [
-            (rf"^\s*#\s*{_block_comment_prefix}", Comment.Multiline, "comments.block.emphasized"),
+            (rf"^\s*#{_block_comment_prefix}", Comment.Multiline, "comments.block.emphasized"),
             (r"^\s*#", Comment.Multiline, "comments.block.normal"),
             default("#pop"),
         ],
         "comments.block.normal": [
-            include("comments.block.param"),
+            include("comments.block.special"),
             (r"\S+?", Comment.Multiline),
             (r"\n", Text, "#pop"),
             include("whitespace"),
         ],
         "comments.block.emphasized": [
-            include("comments.block.param"),
+            include("comments.block.special"),
             (r"\S+", String.Doc),
             (r"\n", Text, "#pop"),
             include("whitespace"),
         ],
-        "comments.block.param": [
+        "comments.block.special": [
+            # Params
             (r"@\S+", Name.Decorator),
+
+            include("resource-name"),
+
+            # Scoreboard player names
+            (r"[#%$][A-Za-z0-9_.#%$]+", Name.Variable.Magic),
         ],
 
         "operators": [
-            (r"[-~%^?!+*<>\\/|&=.]", Operator),
+            (r"[\-~%^?!+*<>\\/|&=.]", Operator),
         ],
 
         "literals": [
             (r"\.\.", Literal),
             (r"(true|false)", Keyword.Pseudo),
-            (r"[a-z_]+", Name.Variable.Class),
+
+            # these are like unquoted strings and appear in many places
+            (r"[A-Za-z_]+", Name.Variable.Class),
+
             (r"[0-7]b", Number.Byte),
             (r"[+-]?\d*\.?\d+([eE]?[+-]?\d+)?[df]?\b", Number.Float),
             (r"[+-]?\d+\b", Number.Integer),
@@ -213,59 +224,103 @@ class MCFunctionLexer(RegexLexer):
         ],
 
         "selectors": [
-            (r"@[a-z](?=\[)", Name.Variable, "selectors-inside"),
             (r"@[a-z]", Name.Variable),
         ],
-        "selectors-inside": [
-            (r"\[", Punctuation),
-            # this checks whether eventually there is an '=' sign
-            #  otherwise, we assume NBT
-            (r"[a-z_]+(?=\=\{\s*[a-z:_\/]+\=)", Name.Tag, "selectors-inside-tag-no-nbt"),
-            (r"[a-z_]+(?=\=)", Name.Tag, "selectors-inside-tag"),
-            (r"[,\.]", Punctuation),
+ 
+
+        ## Generic Property Container
+        # There are several, differing instances where the language accepts
+        #  specific contained keys or contained key, value pairings.
+        # 
+        # Property Maps:
+        # - Starts with either `[` or `{`
+        # - Key separated by `:` or `=`
+        # - Deliminated by `,`
+        # 
+        # Property Lists:
+        # - Starts with `[`
+        # - Deliminated by `,`
+        # 
+        # For simplicity, these patterns match a generic, nestable structure
+        #  which follow a key, value pattern. For normal lists, there's only keys.
+        # This allow some "illegal" structures, but we'll accept those for
+        #  sake of simplicity
+        # 
+        # Examples:
+        # - `[facing=up, powered=true]` (blockstate)
+        # - `[name="hello world", nbt={key: 1b}]` (selector + nbt)
+        # - `[{"text": "value"}, "literal"]` (json)
+        ##
+        "property": [
+            # This state gets included in root and also several substates
+            # We do this to shortcut the starting of new properties
+            #  within other properties. Lists can have sublists and compounds
+            #  and values can start a new property (see the `difficult_1.txt`
+            #  snippet).
+            (r"\{", Punctuation, ("property.curly", "property.key")),
+            (r"\[", Punctuation, ("property.square", "property.key")),
+        ],
+        "property.curly": [
             include("whitespace"),
-            include("literals"),
+            include("property"),
+            (r"\}", Punctuation, "#pop"),
+        ],
+        "property.square": [
+            include("whitespace"),
+            include("property"),
             (r"\]", Punctuation, "#pop"),
-        ],
-        "selectors-inside-tag": [
-            (r"[!=]", Operator),
-            include("nbt"),
-            include("resource-name"),
-            include("whitespace"),
-            include("literals"),
-            default("#pop"),
-        ],
-        "selectors-inside-tag-no-nbt": [
-            (r"[!=,]", Operator),
-            include("resource-name"),
-            include("whitespace"),
-            include("literals"),
-            (r"\{", Token, "#push"),
-            (r"\}", Token, "#pop"),
-            default("#pop"),
-        ],
 
-        # We have to push and pop on these brackets so that we don't
-        #  accidentally consume too many tokens which aren't inside the SNBT.
-        #  If we don't do this logic, we may accidentally pass too many tokens
-        #  to the SNBTLexer.
-        "nbt": [
-            (r"\{", Punctuation, "nbt.compound"),
-            (r"\[", Punctuation, "nbt.list"),
+            # lists can have sequences of items
+            (r",", Punctuation),
         ],
-        "nbt.compound": [
-            #(r"\{", Punctuation.C.MCF, "#push"),
-            (r".+", using(SNBTLexer, state=("compound",))),
-            default("#pop"),
-        ],
-        "nbt.list": [
-            #(r"\[", Punctuation.L.MCF, "#push"),
-            #(r"\]", Punctuation.L.MCF, "#pop"),
-            (r".+", using(SNBTLexer, state=("list",))),
-            default("#pop"),
-        ],
+        "property.key": [
+            include("whitespace"),
 
-        "delimiters": [
-            (r"[,;(){}\[\]:]", Punctuation),
+            # resource names (for advancements)
+            #  can omit `:` to default `minecraft:`
+            # must check if there is a future equals sign if `:` is in the name
+            (r"#?[a-z_][a-z_\.\-]*\:[a-z0-9_\.\-/]+(?=\s*\=)", Name.Attribute, "property.delimiter"),
+            (r"#?[a-z_][a-z0-9_\.\-/]+", Name.Attribute, "property.delimiter"),
+
+            # unquoted NBT key
+            (r"[A-Za-z_\-\+]+", Name.Attribute, "property.delimiter"),
+
+            # quoted JSON or NBT key
+            (r'"', Name.Attribute, "property.delimiter", "literals.string-double"),
+            (r"'", Name.Attribute, "property.delimiter", "literals.string-single"),
+
+            # index for a list
+            (r"-?\d+", Number.Integer, "property.delimiter"),
+
+            default("#pop"),
+        ],
+        "property.key.string-double": [
+            (r"\\.", String.Escape),
+            (r'[^\\"\n]+', Name.Attribute),
+            (r'"', Name.Attribute, "#pop"),
+        ],
+        "property.key.string-single": [
+            (r"\\.", String.Escape),
+            (r"[^\\'\n]+", Name.Attribute),
+            (r"'", Name.Attribute, "#pop"),
+        ],
+        "property.delimiter": [
+            include("whitespace"),
+            
+            (r"[:=]!?", Punctuation, "property.value"),
+            (r",", Punctuation),
+            default("#pop"),
+        ],
+        "property.value": [
+            include("whitespace"),
+
+            # unquoted resource names are valid literals here
+            (r"#?[a-z_][a-z_\.\-]*\:[a-z0-9_\.\-/]+", Name.Tag),
+            (r"#?[a-z_][a-z0-9_\.\-/]+", Name.Tag),
+
+            include("literals"),
+            include("property"),
+
+            default("#pop"),
         ],
     }
