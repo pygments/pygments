81e884ea00b060d23dc796803897fbff74d3785d
mattv52 <matthewvolaris@yahoo.com.au>
2022-05-16 12:28:52 +0930

    Added a rough outline for the new format of lexer

--- pygments/lexers/c_like.py
+++ pygments/lexers/c_like.py
@@ -687,41 +687,65 @@ class FlexLexer(CLexer):
     url = 'http://github.com/westes/flex'
 
     tokens = {
-        # root will be for definitions section and user code (through inherit mainly)
+        # Handle the definitions section
         'root': [
-            include('whitespace'),
-            (r'%%', String.Delimiter, 'rules'),
-            (r'%[{}]', Comment.Preproc),
-            inherit
+        
         ],
-        # rules for rules section
+        # Handle the rules section
         'rules': [
-            include('whitespace'),
-            (r'%%', String.Delimiter, '#pop'),
-            (r'\\[abfnrtv]', String.Char),
-            include('root')
+        
         ],
-        'keywords': [
-            (words((
-                'yytext','yylmax','yyleng','yyin','yylineno','yy_current_buffer', 'yy_break','yy_buffer_state','yy_flex_debug','yy_end_of_buffer_char','yy_user_action','yy_user_init','yy_act','yy_interactive','yy_start','yy_decl','yy_flex_major_version', 'yy_flex_minor_version',
-            )), Name.Builtin),
-            (words((
-                'yylex', 'yymore', 'yyterminate', 'yy_input', 'yyless', 'yycopy', 'yyrestart', 'yy_scan_string', 'yy_scan_bytes', 'yy_scan_buffer', 'yywrap',  'yy_set_interactive','yy_set_bol','yy_at_bol','yy_create_buffer','yy_delete_buffer','yy_flush_buffer', 'yy_flex_debug','yy_init_buffer','yy_flush_buffer','yy_load_buffer_state','yy_switch_to_buffer','yy_pop_state','yy_push_state','yy_top_state','yyFlexLexer'
-            )), Name.Function),
-            inherit,
+        # Handle the user code section
+        'usercode': [
+        
         ],
-        'statements': [
-            (r'\[:(alnum|alpha|blank|cntrl|digit|graph|lower|print|punct|space|upper|xdigit):\]', Name.Builtin),
-            (r'\n', Whitespace, '#pop'),
-            (r'[a-zA-Z]\-[a-zA-Z]', String),
-            (r'\\[ntr]',String.Escape),
-            (words((
-                'if','then','begin','end','procedure','function'), suffix=r'\b'),
-             Keyword),
-            (words(('nx_struct', 'nx_union', 'nx_int8_t', 'nx_int16_t', 'nx_int32_t',
-                    'nx_int64_t', 'nx_uint8_t', 'nx_uint16_t', 'nx_uint32_t',
-                    'nx_uint64_t'), suffix=r'\b'),
-             Keyword.Type),
-            inherit,
+        # Handle whitespace
+        'whitespace': [
+
+        ],
+        # Handle regular expressions
+        'regex': [
+
         ]
     }
+
+
+    # tokens = {
+    #     # root will be for definitions section and user code (through inherit mainly)
+    #     'root': [
+    #         include('whitespace'),
+    #         (r'%%', String.Delimiter, 'rules'),
+    #         (r'%[{}]', Comment.Preproc),
+    #         inherit
+    #     ],
+    #     # rules for rules section
+    #     'rules': [
+    #         include('whitespace'),
+    #         (r'%%', String.Delimiter, '#pop'),
+    #         (r'\\[abfnrtv]', String.Esacpe),
+    #         include('root')
+    #     ],
+    #     'keywords': [
+    #         (words((
+    #             'yytext','yylmax','yyleng','yyin','yylineno','yy_current_buffer', 'yy_break','yy_buffer_state','yy_flex_debug','yy_end_of_buffer_char','yy_user_action','yy_user_init','yy_act','yy_interactive','yy_start','yy_decl','yy_flex_major_version', 'yy_flex_minor_version',
+    #         )), Name.Builtin),
+    #         (words((
+    #             'yylex', 'yymore', 'yyterminate', 'yy_input', 'yyless', 'yycopy', 'yyrestart', 'yy_scan_string', 'yy_scan_bytes', 'yy_scan_buffer', 'yywrap',  'yy_set_interactive','yy_set_bol','yy_at_bol','yy_create_buffer','yy_delete_buffer','yy_flush_buffer', 'yy_flex_debug','yy_init_buffer','yy_flush_buffer','yy_load_buffer_state','yy_switch_to_buffer','yy_pop_state','yy_push_state','yy_top_state','yyFlexLexer'
+    #         )), Name.Function),
+    #         inherit,
+    #     ],
+    #     'statements': [
+    #         (r'\[:(alnum|alpha|blank|cntrl|digit|graph|lower|print|punct|space|upper|xdigit):\]', Name.Builtin),
+    #         (r'\n', Whitespace, '#pop'),
+    #         (r'[a-zA-Z]\-[a-zA-Z]', String),
+    #         (r'\\[abfnrtv]', String.Esacpe),
+    #         (words((
+    #             'if','then','begin','end','procedure','function'), suffix=r'\b'),
+    #          Keyword),
+    #         (words(('nx_struct', 'nx_union', 'nx_int8_t', 'nx_int16_t', 'nx_int32_t',
+    #                 'nx_int64_t', 'nx_uint8_t', 'nx_uint16_t', 'nx_uint32_t',
+    #                 'nx_uint64_t'), suffix=r'\b'),
+    #          Keyword.Type),
+    #         inherit,
+    #     ]
+    # }
