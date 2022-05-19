9110ad7993684deb0e785b181c4ca25b0c2a1e55
Amit <amit.kummer@gmail.com>
2022-05-19 21:28:25 +0300

    Update demo to work with Pyodide v0.20
    Remove usage of Pyodide functionality which was deprecated in Pyodide v0.17:
    
    - Loading Pyodide using `languagePluginURL` and `languagePluginLoader`.
    - Access to globals via `pyodide.globals.x` has changed to `pyodide.globals.get("x")`.
    
    Source:
    https://pyodide.org/en/stable/project/release-notes/v0.17.0.html?highlight=languageplugin#api-changes

--- doc/_static/demo.js
+++ doc/_static/demo.js
@@ -29,7 +29,7 @@ if (qvars.formatter !== undefined) {
 }
 
 styleSelect.addEventListener('change', () => {
-    style.textContent = styles[styleSelect.value];
+    style.textContent = styles.get(styleSelect.value);
     updateCopyLink();
     updateContrastWarning();
 });
@@ -89,7 +89,7 @@ highlightWorker.onmessage = (msg) => {
         outputDiv.innerHTML = msg.data.html;
         codeHeader.hidden = false;
         loadingDiv.hidden = true;
-        style.textContent = styles[styleSelect.value];
+        style.textContent = styles.get(styleSelect.value);
     } else if (msg.data.tokens) {
         const table = document.createElement('table');
         table.className = 'tokens';
