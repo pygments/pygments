33a525ccd4d55bafe4a7e4b4c09108f9e79c6a65
mattv52 <matthewvolaris@yahoo.com.au>
2022-05-23 11:35:00 +0930

    Chanegd some tokens and removed old commented out code

--- pygments/lexers/c_like.py
+++ pygments/lexers/c_like.py
@@ -691,8 +691,8 @@ class FlexLexer(RegexLexer):
         'root': [
             (r'(%[{}])', String.Delimiter),
             (r'%%', String.Delimiter, 'rules'),
-            (r'(#include)(\s)(<\w*\.*\w*>)', bygroups(Keyword, Whitespace, Name)),
-            (r'(#define)(\s)(\w*\.*\w*)', bygroups(Keyword, Whitespace, Name)),
+            (r'(#include)(\s)(<\w*\.*\w*>)', bygroups(Comment.Preproc, Whitespace, Comment.PreprocFile)),
+            (r'(#define)(\s)(\w*\.*\w*)', bygroups(Comment.Preproc, Whitespace, Name)),
             (words(('bool', 'int', 'long', 'float', 'short', 'double', 'char',
                 'unsigned', 'signed', 'void'), suffix=r'\b'), Keyword.Type),
             (r'\n', Whitespace),
@@ -752,26 +752,3 @@ class FlexLexer(RegexLexer):
             (r'.', String),
         ],
     }
-
-
-    # tokens = {
-    #     'keywords': [
-    #         (words((
-    #             'yytext','yylmax','yyleng','yyin','yylineno','yy_current_buffer', 'yy_break','yy_buffer_state','yy_flex_debug','yy_end_of_buffer_char','yy_user_action','yy_user_init','yy_act','yy_interactive','yy_start','yy_decl','yy_flex_major_version', 'yy_flex_minor_version',
-    #         )), Name.Builtin),
-    #         (words((
-    #             'yylex', 'yymore', 'yyterminate', 'yy_input', 'yyless', 'yycopy', 'yyrestart', 'yy_scan_string', 'yy_scan_bytes', 'yy_scan_buffer', 'yywrap',  'yy_set_interactive','yy_set_bol','yy_at_bol','yy_create_buffer','yy_delete_buffer','yy_flush_buffer', 'yy_flex_debug','yy_init_buffer','yy_flush_buffer','yy_load_buffer_state','yy_switch_to_buffer','yy_pop_state','yy_push_state','yy_top_state','yyFlexLexer'
-    #         )), Name.Function),
-    #         inherit,
-    #     ],
-    #     'statements': [
-    #         (words((
-    #             'if','then','begin','end','procedure','function'), suffix=r'\b'),
-    #          Keyword),
-    #         (words(('nx_struct', 'nx_union', 'nx_int8_t', 'nx_int16_t', 'nx_int32_t',
-    #                 'nx_int64_t', 'nx_uint8_t', 'nx_uint16_t', 'nx_uint32_t',
-    #                 'nx_uint64_t'), suffix=r'\b'),
-    #          Keyword.Type),
-    #         inherit,
-    #     ]
-    # }
