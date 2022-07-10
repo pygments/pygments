1db972cf0f7937cdeee95767463d415672872b69
flywire <flywire0@gmail.com>
2022-07-10 16:36:50 +1000

    Update staroffice.py colors for WCAG 2.1 contrast

--- pygments/styles/staroffice.py
+++ pygments/styles/staroffice.py
@@ -9,20 +9,16 @@
 """
 
 from pygments.style import Style
-from pygments.token import Keyword, Name, Comment, String, Error, \
-     Number, Operator, Generic
+from pygments.token import Comment, Error, Literal, Name
 
 class StarofficeStyle(Style):
     """
     Style similar to StarOffice style, also in OpenOffice and LibreOffice.
     """
-    default_style = ""
+    default_style =             '#000080'    # Blue
     styles = {
-        Comment:                '#666666',   # Gray
-        Error:                  '#800000',   # Red
-        Keyword:                '#000080',   # Blue
+        Comment:                '#696969',   # DimGray
+        Error:                  '#800000',   # Maroon
+        Literal:                '#EE0000',   # Red
         Name:                   '#008000',   # Green
-        Number:                 '#FF0000',   # Lightred
-        Operator:               '#000080',   # Blue
-        String:                 '#FF0000',   # Lightred
     }
