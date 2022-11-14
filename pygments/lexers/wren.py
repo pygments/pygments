f982b0ae6e5f1151473a15c9163b5fac7f81bc50
PureFox48 <64583745+PureFox48@users.noreply.github.com>
2022-11-14 09:32:05 +0000

    Update wren.py
    Corrected module docstring.

--- pygments/lexers/wren.py
+++ pygments/lexers/wren.py
@@ -1,8 +1,9 @@
-
 """
     pygments.lexers.wren
     ~~~~~~~~~~~~~~~~~~~~
+    
     Lexer for Wren.
+    
     :copyright: Copyright 2006-2022 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
