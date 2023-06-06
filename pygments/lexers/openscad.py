61075c0497010a39cd7aca312c5adb4873f6f322
Nikolay Antipov <nantipov@users.noreply.github.com>
2023-06-06 08:20:32 +0200

    Format and doc adjustments (#886)
    Co-authored-by: Jean Abou-Samra <jean@abou-samra.fr>

--- pygments/lexers/openscad.py
+++ pygments/lexers/openscad.py
@@ -15,6 +15,10 @@ __all__ = ['OpenScadLexer']
 
 
 class OpenScadLexer(RegexLexer):
+    """For openSCAD code.
+    
+    .. versionadded:: 2.16.0
+    """
     name = "OpenSCAD"
     aliases = ["openscad"]
     filenames = ["*.scad"]
@@ -23,8 +27,8 @@ class OpenScadLexer(RegexLexer):
         "root": [
             (r"[^\S\n]+", Text),
             (r"\n", Text),
-            (r"//(\n|[\w\W]*?[^\\]\n)", Comment.Singleline),
-            (r"/(\\\n)?[*][\w\W]*?[*](\\\n)?/", Comment.Mutliline),
+            (r"//(.|\n)*?(?<!\\)\n", Comment.Single),
+            (r"/(\\\n)?[*](.|\n)*?[*](\\\n)?/", Comment.Mutliline),
             (r"/(\\\n)?[*][\w\W]*", Comment.Mutliline),
             (r"[{}\[\]\(\),;:]", Punctuation),
             (r"[*!#%\-+=?/]", Operator),
@@ -36,25 +40,19 @@ class OpenScadLexer(RegexLexer):
                 bygroups(Keyword.Namespace, Text),
                 "includes",
             ),
-            (
-                r"(module)(\s*)([^\s\(]+)",
-                bygroups(Keyword.Namespace, Text, Name.Namespace),
-            ),
-            (
-                r"(function)(\s*)([^\s\(]+)",
-                bygroups(Keyword.Declaration, Text, Name.Function),
-            ),
+            (r"(module)(\s*)([^\s\(]+)",
+             bygroups(Keyword.Namespace, Whitespace, Name.Namespace)),
+            (r"(function)(\s*)([^\s\(]+)",
+             bygroups(Keyword.Declaration, Whitespace, Name.Function)),
             (r"\b(true|false)\b", Literal),
-            (
-                r"\b(function|module|include|use|for|intersection_for|if|else|return)\b",
-                Keyword,
-            ),
+            (r"\b(function|module|include|use|for|intersection_for|if|else|return)\b",
+             Keyword),
             (
                 r"\b(circle|square|polygon|text|sphere|cube|cylinder|polyhedron|translate|rotate|scale|resize|mirror|multmatrix|color|offset|hull|minkowski|union|difference|intersection|abs|sign|sin|cos|tan|acos|asin|atan|atan2|floor|round|ceil|ln|log|pow|sqrt|exp|rands|min|max|concat|lookup|str|chr|search|version|version_num|norm|cross|parent_module|echo|import|import_dxf|dxf_linear_extrude|linear_extrude|rotate_extrude|surface|projection|render|dxf_cross|dxf_dim|let|assign|len)\b",
                 Name.Builtin,
             ),
             (r"\bchildren\b", Name.Builtin.Pseudo),
-            (r"&#34;(\\\\|\\&#34;|[^&#34;])*&#34;", Literal.StringDouble),
+            (r"&#34;(\\\\|\\&#34;|[^&#34;])*&#34;", String.Double),
             (r"-?\d+(\.\d+)?(e[+-]?\d+)?", Literal.Number),
             (r"[a-zA-Z_]\w*", Name),
         ],
