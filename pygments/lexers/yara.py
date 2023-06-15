6fe1499156ef6bfc95d64c00a28c1eb1f9f98d62
redteampanda-ng <31235211+redteampanda-ng@users.noreply.github.com>
2023-06-15 15:53:44 +0200

    Update pygments/lexers/yara.py
    Co-authored-by: Jean Abou-Samra <jean@abou-samra.fr>

--- pygments/lexers/yara.py
+++ pygments/lexers/yara.py
@@ -37,7 +37,7 @@ class YaraLexer(RegexLexer):
             (words(('ascii', 'at', 'base64', 'base64wide', 'condition', 'contains', 'endswith', 'entrypoint', 'filesize', 'for', 'fullword', 'icontains', 'iendswith', 'iequals', 'in', 'include', 'int16', 'int16be', 'int32', 'int32be', 'int8', 'int8be', 'istartswith', 'matches', 'meta', 'nocase', 'none', 'of', 'startswith', 'strings', 'them', 'uint16', 'uint16be', 'uint32', 'uint32be', 'uint8', 'uint8be', 'wide', 'xor', 'defined'), prefix=r'\b', suffix=r'\b'), Generic.Strong),
             (r'(true|false)\b', Keyword.Constant),
             (r'(and|or|not|any|all)\b', Operator.Word),
-            (r'(\$[\w\d]+)', Name.Variable),
+            (r'(\$\w+)', Name.Variable),
             (r'"[^"]*"', String.Double),
             (r'\'[^\']*\'', String.Single),
             (r'(\{.*?\})$', Number.Hex),
