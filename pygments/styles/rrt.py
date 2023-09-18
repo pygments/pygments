ac424e81fe16f6c4e95a4f1f743f79ffff583f55
Reuben Thomas <rrt@sc3d.org>
2023-09-19 02:30:08 +0300

    rrt style: add Number color

--- pygments/styles/rrt.py
+++ pygments/styles/rrt.py
@@ -9,7 +9,7 @@
 """
 
 from pygments.style import Style
-from pygments.token import Token, Comment, Name, Keyword, String
+from pygments.token import Token, Comment, Name, Keyword, String, Number
 
 
 __all__ = ['RrtStyle']
@@ -21,7 +21,7 @@ class RrtStyle(Style):
     """
 
     name = 'rrt'
-    
+
     background_color = '#000000'
     highlight_color = '#0000ff'
 
@@ -35,4 +35,5 @@ class RrtStyle(Style):
         Comment.Preproc:    '#e5e5e5',
         String:             '#87ceeb',
         Keyword.Type:       '#ee82ee',
+        Number:             '#ff00ff',
     }
