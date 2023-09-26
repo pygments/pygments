794e14cb50bbfb69d903d1c67bd59cfff23779ba
Flavio Curella <89607+fcurella@users.noreply.github.com>
2023-09-26 09:31:28 -0500

    Update tokens
    Co-authored-by: Jean Abou-Samra <jean@abou-samra.fr>

--- pygments/lexers/jsx.py
+++ pygments/lexers/jsx.py
@@ -12,63 +12,59 @@ import re
 
 from pygments.lexer import bygroups, default, include, inherit
 from pygments.lexers.javascript import JavascriptLexer
-from pygments.token import Name, Operator, Punctuation, String, Text
+from pygments.token import Name, Operator, Punctuation, String, Text, Whitespace
 
 __all__ = ['JsxLexer']
 
-# Use same tokens as `JavascriptLexer`, but with tags and attributes support
-TOKENS = {
-    "root": [
-        include("jsx"),
-        inherit,
-    ],
-    "jsx": [
-        (
-            r"(<)(/?)(>)",
-            bygroups(Punctuation, Punctuation, Punctuation),
-        ),  # JSXFragment <>|</>
-        (r"(<)([\w]+)(\.?)", bygroups(Punctuation, Name.Tag, Punctuation), "tag"),
-        (
-            r"(<)(/)([\w]+)(>)",
-            bygroups(Punctuation, Punctuation, Name.Tag, Punctuation),
-        ),
-        (
-            r"(<)(/)([\w]+)",
-            bygroups(Punctuation, Punctuation, Name.Tag),
-            "fragment",
-        ),  # Same for React.Context
-    ],
-    "tag": [
-        (r"\s+", Text),
-        (r"([\w-]+\s*)(=)(\s*)", bygroups(Name.Attribute, Operator, Text), "attr"),
-        (r"[{}]+", Punctuation),
-        (r"[\w\.]+", Name.Attribute),
-        (r"(/?)(\s*)(>)", bygroups(Punctuation, Text, Punctuation), "#pop"),
-    ],
-    "fragment": [
-        (r"(.)([\w]+)", bygroups(Punctuation, Name.Attribute)),
-        (r"(>)", bygroups(Punctuation), "#pop"),
-    ],
-    "attr": [
-        (r"\{", Punctuation, "expression"),
-        (r'".*?"', String, "#pop"),
-        (r"'.*?'", String, "#pop"),
-        default("#pop"),
-    ],
-    "expression": [
-        (r"\{", Punctuation, "#push"),
-        (r"\}", Punctuation, "#pop"),
-        include("root"),
-    ],
-}
-
 
 class JsxLexer(JavascriptLexer):
-    name = "react"
+    name = "JSX"
     aliases = ["jsx", "react"]
     filenames = ["*.jsx", "*.react"]
     mimetypes = ["text/jsx", "text/typescript-jsx"]
+    url = "https://react.dev"
 
-    flags = re.MULTILINE | re.DOTALL | re.UNICODE
+    flags = re.MULTILINE | re.DOTALL
 
-    tokens = TOKENS
+    # Use same tokens as `JavascriptLexer`, but with tags and attributes support
+    tokens = {
+        "root": [
+            include("jsx"),
+            inherit,
+        ],
+        "jsx": [
+            (r"</?>", Punctuation),  # JSXFragment <>|</>
+            (r"(<)(\w+)(\.?)", bygroups(Punctuation, Name.Tag, Punctuation), "tag"),
+            (
+                r"(</)(\w+)(>)",
+                bygroups(Punctuation, Name.Tag, Punctuation),
+            ),
+            (
+                r"(</)(\w+)",
+                bygroups(Punctuation, Name.Tag),
+                "fragment",
+            ),  # Same for React.Context
+        ],
+        "tag": [
+            (r"\s+", Whitespace),
+            (r"([\w-]+)(\s*)(=)(\s*)", bygroups(Name.Attribute, Whitespace, Operator, Whitespace), "attr"),
+            (r"[{}]+", Punctuation),
+            (r"[\w\.]+", Name.Attribute),
+            (r"(/?)(\s*)(>)", bygroups(Punctuation, Text, Punctuation), "#pop"),
+        ],
+        "fragment": [
+            (r"(.)(\w+)", bygroups(Punctuation, Name.Attribute)),
+            (r"(>)", bygroups(Punctuation), "#pop"),
+        ],
+        "attr": [
+            (r"\{", Punctuation, "expression"),
+            (r'".*?"', String, "#pop"),
+            (r"'.*?'", String, "#pop"),
+            default("#pop"),
+        ],
+        "expression": [
+            (r"\{", Punctuation, "#push"),
+            (r"\}", Punctuation, "#pop"),
+            include("root"),
+        ],
+    }
