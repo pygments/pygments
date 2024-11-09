1daa694d9faabf2a0648b804fe0ab5c25815b943
Josef Meyer <20089460@tafe.wa.edu.au>
2024-11-10 02:04:51 +0800

    test: adds a test for issue #2692
    This issue involves the xml lexer mistagging a DOCTYPE containing
    an inline DTD.  The closing part of the DOCTYPE is recognised as
    `Text`.
    
    The test added calls `pytest.xfail` if it detects the situation
    described when processing the sample of code provided:
    
    ```xml
    <?xml version="1.0" encoding="UTF-8"?>
    <!DOCTYPE hello [<!ELEMENT hello (#PCDATA)>]>
    <hello>Hello Word!</hello>
    ```
    
    The test is currently slightly fragile, in that it will raise
    an exception if "]>" is not part of a single token; this is
    easily fixed, but does not cause a problem with the current lexer,
    and is unlikely to do so in the future.

--- tests/test_html_formatter.py
+++ tests/test_html_formatter.py
@@ -17,7 +17,9 @@ import pytest
 from pygments.formatters import HtmlFormatter, NullFormatter
 from pygments.formatters.html import escape_html
 from pygments.lexers import PythonLexer
+from pygments.lexers.html import XmlLexer
 from pygments.style import Style
+from pygments.token import Text
 
 TESTDIR = path.dirname(path.abspath(__file__))
 TESTFILE = path.join(TESTDIR, 'test_html_formatter.py')
@@ -269,3 +271,26 @@ def test_debug_token_types():
     fmt_debug_token_types.format(tokensource, outfile_debug_token_types)
     html_debug_token_types = outfile_debug_token_types.getvalue()
     assert '<span class="n" title="Name">TESTDIR</span>' in html_debug_token_types
+
+
+def test_close_of_doctype_highlighted_as_normal_text():
+    """Addresses #2692: incorrect highlighting at the end of doctype.
+
+    This specifically tests for the reported issue; that is, ']>' in
+    a DOCTYPE containing an inline DTD being tagged as `Text`.
+    """
+    sample_code = """
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE hello [<!ELEMENT hello (#PCDATA)>]>
+        <hello>Hello Word!</hello>
+        """
+
+    lexer = XmlLexer()
+
+    # this will raise an error if ']>' is not part of a single token:
+    closing_token = next(
+        token for token, text in lexer.get_tokens(sample_code) if ']>' in text
+    )
+
+    if closing_token == Text:
+        pytest.xfail("Unfixed issue parsing DOCTYPEs containing inline DTDs.")
