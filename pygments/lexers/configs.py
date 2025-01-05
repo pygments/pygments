pygments/pygments

commit 2dad7c0e1c83cace532ee469c885e7d07ed4a894

Fix quoted string containing comment character
The comment character ('#' or ';') included in a quoted value does not be interpreted as beginning of comment but as a character of the quoted value.

pygments/lexers/configs.py
@@ -44,6 +44,9 @@ class IniLexer(RegexLexer):
             (r'\s+', Whitespace),
             (r'[;#].*', Comment.Single),
             (r'(\[.*?\])([ \t]*)$', bygroups(Keyword, Whitespace)),
+            (r'''(.*?)([  \t]*)([=:])([ \t]*)(["'])''',
+             bygroups(Name.Attribute, Whitespace, Operator, Whitespace, String),
+             "quoted_value"),
             (r'(.*?)([  \t]*)([=:])([ \t]*)([^;#\n]*)(\\)(\s+)',
              bygroups(Name.Attribute, Whitespace, Operator, Whitespace, String,
                       Text, Whitespace),
@@ -52,6 +55,12 @@ class IniLexer(RegexLexer):
              bygroups(Name.Attribute, Whitespace, Operator, Whitespace, String)),
             # standalone option, supported by some INI parsers
             (r'(.+?)$', Name.Attribute),
+        ],
+        'quoted_value': [
+            (r'''([^"'\n]*)(["'])(\s*)''',
+             bygroups(String, String, Whitespace)),
+            (r'[;#].*', Comment.Single),
+            (r'$', String, "#pop"),
         ],
         'value': [     # line continuation
             (r'\s+', Whitespace),

commit 673d8243c1c66363c83ae5467b4cecf13b506f3c

Fix quoted string handling
Commit 2dad7c0e introduced regression in the handling of strings with
double quotes.

The bug was even "featured" in the golden for test_quoted_entries.txt
since `key 2` attribute was improperly parsed as literal string "key 2 = ".

Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

pygments/lexers/configs.py
@@ -44,21 +44,21 @@ class IniLexer(RegexLexer):
             (r'\s+', Whitespace),
             (r'[;#].*', Comment.Single),
             (r'(\[.*?\])([ \t]*)$', bygroups(Keyword, Whitespace)),
-            (r'''(.*?)([  \t]*)([=:])([ \t]*)(["'])''',
+            (r'''(.*?)([ \t]*)([=:])([ \t]*)(["'])''',
              bygroups(Name.Attribute, Whitespace, Operator, Whitespace, String),
              "quoted_value"),
-            (r'(.*?)([  \t]*)([=:])([ \t]*)([^;#\n]*)(\\)(\s+)',
+            (r'(.*?)([ \t]*)([=:])([ \t]*)([^;#\n]*)(\\)(\s+)',
              bygroups(Name.Attribute, Whitespace, Operator, Whitespace, String,
                       Text, Whitespace),
              "value"),
-            (r'(.*?)([ \t]*)([=:])([  \t]*)([^ ;#\n]*(?: +[^ ;#\n]+)*)',
+            (r'(.*?)([ \t]*)([=:])([ \t]*)([^ ;#\n]*(?: +[^ ;#\n]+)*)',
              bygroups(Name.Attribute, Whitespace, Operator, Whitespace, String)),
             # standalone option, supported by some INI parsers
             (r'(.+?)$', Name.Attribute),
         ],
         'quoted_value': [
             (r'''([^"'\n]*)(["'])(\s*)''',
-             bygroups(String, String, Whitespace)),
+             bygroups(String, String, Whitespace), "#pop"),
             (r'[;#].*', Comment.Single),
             (r'$', String, "#pop"),
         ],