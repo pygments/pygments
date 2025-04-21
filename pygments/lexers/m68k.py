875554e86aa44540be292516ae53f13ad5cba594
ggelado <144947535+ggelado@users.noreply.github.com>
2025-04-21 10:50:58 +0200

    HEADER

--- pygments/lexers/m68k.py
+++ pygments/lexers/m68k.py
@@ -1,7 +1,20 @@
-#author: ggelado
-# LICENSE: GPL
-# there is no warranty for this free software
-# author: ggelado
+# M68K Assembly Lexer for Pygments
+# Copyright (C) 2025  ggelado
+#
+# Licensed under the EUPL, Version 1.2 or – as soon they will be
+# approved by the European Commission – subsequent versions of the
+# EUPL (the "Licence").
+#
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at:
+#
+#     https://eupl.eu/
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the Licence is distributed on an "AS IS"
+# basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the Licence for the specific language governing
+# permissions and limitations under the Licence.
 from pygments.lexer import RegexLexer
 from pygments.token import *
 import re
