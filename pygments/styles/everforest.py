c271dd07bab8c8acc15160421557beb4f7605526
Philippe Massicotte <pmassicotte@hotmail.com>
2025-06-10 08:06:43 -0400

    style: remove redundant color name comments in everforest style definitions

--- pygments/styles/everforest.py
+++ pygments/styles/everforest.py
@@ -33,47 +33,47 @@ class EverforestDarkStyle(Style):
     line_number_color = "#7a8478"  # grey1
 
     styles = {
-        Comment: "italic #859289",  # grey2
-        Comment.Preproc: "noitalic #7fbbb3",  # blue
-        Comment.Special: "noitalic #e67e80",  # red
-        Keyword: "bold #a7c080",  # green
-        Keyword.Pseudo: "nobold #a7c080",  # green
-        Keyword.Type: "nobold #d699b6",  # purple
-        Operator: "#e69875",  # orange
-        Operator.Word: "bold #a7c080",  # green
-        Name.Builtin: "#7fbbb3",  # blue
-        Name.Function: "#a7c080",  # green
-        Name.Class: "bold #dbbc7f",  # yellow
-        Name.Namespace: "bold #dbbc7f",  # yellow
-        Name.Exception: "#e67e80",  # red
-        Name.Variable: "#d699b6",  # purple
-        Name.Constant: "#d699b6",  # purple
-        Name.Label: "bold #d3c6aa",  # fg
-        Name.Entity: "bold #e67e80",  # red
-        Name.Attribute: "#7fbbb3",  # blue
-        Name.Tag: "bold #a7c080",  # green
-        Name.Decorator: "bold #e69875",  # orange
-        String: "#83c092",  # aqua
-        String.Doc: "italic #83c092",  # aqua
-        String.Interpol: "italic #d3c6aa",  # fg
-        String.Escape: "bold #83c092",  # aqua
-        String.Regex: "#83c092",  # aqua
-        String.Symbol: "#d699b6",  # purple
-        String.Other: "#e69875",  # orange
-        Number: "#d699b6",  # purple
-        Punctuation: "#d3c6aa",  # fg
-        Generic.Heading: "bold #d3c6aa",  # fg
-        Generic.Subheading: "bold #d3c6aa",  # fg
-        Generic.Deleted: "#e67e80",  # red
-        Generic.Inserted: "#a7c080",  # green
-        Generic.Error: "#e67e80",  # red
+        Comment: "italic #859289",
+        Comment.Preproc: "noitalic #7fbbb3",
+        Comment.Special: "noitalic #e67e80",
+        Keyword: "bold #a7c080",
+        Keyword.Pseudo: "nobold #a7c080",
+        Keyword.Type: "nobold #d699b6",
+        Operator: "#e69875",
+        Operator.Word: "bold #a7c080",
+        Name.Builtin: "#7fbbb3",
+        Name.Function: "#a7c080",
+        Name.Class: "bold #dbbc7f",
+        Name.Namespace: "bold #dbbc7f",
+        Name.Exception: "#e67e80",
+        Name.Variable: "#d699b6",
+        Name.Constant: "#d699b6",
+        Name.Label: "bold #d3c6aa",
+        Name.Entity: "bold #e67e80",
+        Name.Attribute: "#7fbbb3",
+        Name.Tag: "bold #a7c080",
+        Name.Decorator: "bold #e69875",
+        String: "#83c092",
+        String.Doc: "italic #83c092",
+        String.Interpol: "italic #d3c6aa",
+        String.Escape: "bold #83c092",
+        String.Regex: "#83c092",
+        String.Symbol: "#d699b6",
+        String.Other: "#e69875",
+        Number: "#d699b6",
+        Punctuation: "#d3c6aa",
+        Generic.Heading: "bold #d3c6aa",
+        Generic.Subheading: "bold #d3c6aa",
+        Generic.Deleted: "#e67e80",
+        Generic.Inserted: "#a7c080",
+        Generic.Error: "#e67e80",
         Generic.Emph: "italic",
         Generic.Strong: "bold",
         Generic.EmphStrong: "bold italic",
-        Generic.Prompt: "bold #e69875",  # orange
-        Generic.Output: "#d3c6aa",  # fg
-        Generic.Traceback: "#e67e80",  # red
-        Error: "border:#e67e80",  # red
+        Generic.Prompt: "bold #e69875",
+        Generic.Output: "#d3c6aa",
+        Generic.Traceback: "#e67e80",
+        Error: "border:#e67e80",
     }
 
 
