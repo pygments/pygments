1ebc7177f6e6110be8dd686f3a2554f94d33894e
Alex Jeffery <apjeffery136@gmail.com>
2025-06-20 09:58:17 +0100

    Improve rell lexer, add test file.

--- pygments/lexers/rell.py
+++ pygments/lexers/rell.py
@@ -24,72 +24,25 @@ class RellLexer(RegexLexer):
     mimetypes = ['text/x-rell']
     version_added = '2.19.2'
 
-    reserved_words =
-        reserved_words_module + \
-        reserved_words_control + \
-        reserved_words_branch + \
-        reserved_words_definition + \
-        reserved_words_constant + \
-        reserved_words_boolop + \
-        reserved_words_type + \
-        reserved_words_database + \
-        reserved_words_other + \
-        reserved_words_todo
-
-    reserved_words_module = (
-        'import', 'module',
-    )
-
-    reserved_words_control = (
-        'break', 'continue', 'for', 'return', 'when', 'while',
-    )
-
-    reserved_words_branch = (
-        'if', 'else',
-    )
-
-    reserved_words_definition = (
-        'entity', 'enum', 'function', 'namespace', 'object', 'operation',
-        'query', 'record', 'struct', 'val', 'var',
-    )
-
-    reserved_words_boolop = (
-        'and', 'not', 'or',
-    )
-
-    reserved_words_database = (
-        'create', 'delete', 'index', 'limit', 'offset', 'update',
-    )
-
-    reserved_words_other = (
-        'abstract', 'class', 'guard', 'in', 'include', 'key', 'override',
-    )
-    )
-
     tokens = {
         'root': [
             (r'//.*?$', Comment.Single),
             (r'/\*.*?\*/', Comment.Multiline)
-            (r'"', String, 'string_dq'),
-            (r'\'', String, 'string_sq'),
-            (r'x(\'[0-9]*\'|"[0-9]*")', String, 'byte_array'),
+            (r'"(\\\\|\\"|[^"])*"', String.Double),
+            (r'"(\\\\|\\'|[^\'])*"', String.Single),
+            (r'x(\'[a-fA-F0-9]*\'|"[a-fA-F0-9]*")', String, 'byte_array'),
+            (r'[a-zA-Z_][a-zA-Z0-9_]*', Name.Variable),
+            (r'@[a-zA-Z_][a-zA-Z0-9_]*', Name.Decorator),
+            (r'-?[0-9]+L?', Number.Integer),
+            (r'-?[0-9]*.[0-9]+([eE][+-][0-9]+)?', Number.Float),
             (r'(big_integer|boolean|byte_array|collection|decimal|gtv|integer|'
              r'iterable|json|list|map|mutable|set|text|virtual)\b',
              Keyword.Type),
             (r'(false|true|null)\b', Keyword.Constant),
-        ],
-        'string_dq': [
-            (r'[^\\"]+', String),
-            (r'\\\\', String),  # Escaped backslash
-            (r'\\"', String),  # Escaped quote
-            (r'\\', String),  # Bare backslash
-            (r'"', String, '#pop'),  # Closing quote
-        ],
-        'string_sq': [
-            (r'[^\\\']+', String),
-            (r'\\\\', String),  # Escaped backslash
-            (r'\\\'', String),  # Escaped quote
-            (r'\\', String),  # Bare backslash
-            (r'\'', String, '#pop'),  # Closing quote
+            (r'(entity|enum|function|namespace|object|operation|query|record|'
+             r'struct)\b', Keyword.Declaration),
+            (r'(abstract|and|break|class|continue|create|delete|else|for|guard|'
+             r'if|import|in|include|index|key|limit|module|not|offset|or|'
+             r'override|return|update|val|var|when|while)\b', Keyword.Reserved),
         ],
     }
