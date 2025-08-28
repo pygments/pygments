f09610de3eae11e8e1299f300aa536f3ac6bbe42
Sven Tangermann <s8svtang@stud.uni-saarland.de>
2025-08-28 14:51:19 +0200

    Revert "add older fragments of PDDL to PDDL Lexer"
    This reverts commit 5a1f18efa1a6c24aa13c6019ea3fb1a5424a9cc5.

--- pygments/lexers/pddl.py
+++ pygments/lexers/pddl.py
@@ -52,7 +52,6 @@ class PddlLexer(RegexLexer):
                 ':durative-action', ':duration', ':condition',
                 ':derived', ':domain', ':objects', ':init',
                 ':goal', ':metric', ':length', ':serial', ':parallel',
-                ':axiom', ':implies', ':vars', ':context', ':timeless',
                 # the following are requirements
                 ':strips', ':typing', ':negative-preconditions',
                 ':disjunctive-preconditions', ':equality',
