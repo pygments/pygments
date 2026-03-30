8c031c77f8d3f192b9012e27f3a8fcd2dfac9b56
Styx0x6 <styx0x6@users.noreply.github.com>
2026-03-30 18:08:20 +0000

    Better way to handle None object

--- pygments/formatters/html.py
+++ pygments/formatters/html.py
@@ -424,14 +424,17 @@ class HtmlFormatter(Formatter):
         self.nowrap = get_bool_opt(options, 'nowrap', False)
         self.noclasses = get_bool_opt(options, 'noclasses', False)
         self.classprefix = options.get('classprefix', '')
-        self.cssclass = html.escape(str(self._decodeifneeded(options.get('cssclass', 'highlight'))))
-        self.cssstyles = html.escape(str(self._decodeifneeded(options.get('cssstyles', ''))))
+        _cssclass = self._decodeifneeded(options.get('cssclass', 'highlight'))
+        self.cssclass = html.escape(_cssclass) if _cssclass else _cssclass
+        _cssstyles = self._decodeifneeded(options.get('cssstyles', ''))
+        self.cssstyles = html.escape(_cssstyles) if _cssstyles else _cssstyles
         self.prestyles = self._decodeifneeded(options.get('prestyles', ''))
         self.cssfile = self._decodeifneeded(options.get('cssfile', ''))
         self.noclobber_cssfile = get_bool_opt(options, 'noclobber_cssfile', False)
         self.tagsfile = self._decodeifneeded(options.get('tagsfile', ''))
         self.tagurlformat = self._decodeifneeded(options.get('tagurlformat', ''))
-        self.filename = html.escape(str(self._decodeifneeded(options.get('filename', ''))))
+        _filename = self._decodeifneeded(options.get('filename', ''))
+        self.filename = html.escape(_filename) if _filename else _filename
         self.wrapcode = get_bool_opt(options, 'wrapcode', False)
         self.span_element_openers = {}
         self.debug_token_types = get_bool_opt(options, 'debug_token_types', False)
@@ -454,9 +457,12 @@ class HtmlFormatter(Formatter):
         self.linenostep = abs(get_int_opt(options, 'linenostep', 1))
         self.linenospecial = abs(get_int_opt(options, 'linenospecial', 0))
         self.nobackground = get_bool_opt(options, 'nobackground', False)
-        self.lineseparator = html.escape(str(options.get('lineseparator', '\n')))
-        self.lineanchors = html.escape(str(options.get('lineanchors', '')))
-        self.linespans = html.escape(str(options.get('linespans', '')))
+        _lineseparator = options.get('lineseparator', '\n')
+        self.lineseparator = html.escape(_lineseparator) if _lineseparator else _lineseparator
+        _lineanchors = options.get('lineanchors', '')
+        self.lineanchors = html.escape(_lineanchors) if _lineanchors else _lineanchors
+        _linespans = options.get('linespans', '')
+        self.linespans = html.escape(_linespans) if _linespans else _linespans
         self.anchorlinenos = get_bool_opt(options, 'anchorlinenos', False)
         self.hl_lines = set()
         for lineno in get_list_opt(options, 'hl_lines', []):
